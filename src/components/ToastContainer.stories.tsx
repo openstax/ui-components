1effa3e6a4f42af5d3d72ac8aa61de61b5cf7c53
Josiah Ivey <josiah.ivey@gmail.com>
2022-11-09 18:40:08 -0800

    fix props for story

--- src/components/ToastContainer.stories.tsx
+++ src/components/ToastContainer.stories.tsx
@@ -3,7 +3,7 @@ import { ToastContainer } from './ToastContainer';
 
 const toasts: ToastData[] = [
   { title: 'Neutral', message: 'message' },
-  { title: 'Error', message: 'message', variant: 'failure', dismissAfterMilliseconds: 4000 },
-  { title: 'Success', message: 'message', variant: 'success', dismissAfterMilliseconds: 2000 },
+  { title: 'Error', message: 'message', variant: 'failure', dismissAfterMs: 4000 },
+  { title: 'Success', message: 'message', variant: 'success', dismissAfterMs: 2000 },
 ];
 export const Default = () => <ToastContainer toasts={toasts} />;
