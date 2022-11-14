aa752cb7f8c1a4f1899b74a022638562c81c4e79
staxly <staxly@users.noreply.github.com>
2022-11-14 21:33:47 +0000

    building refs/heads/toasts/343aa52766e2ad4f047436cb0e13cec1ed80a82f

---
+++ refs/heads/toasts/343aa52766e2ad4f047436cb0e13cec1ed80a82f/assets/Button.dcfdaef8.js
@@ -0,0 +1,29 @@
+import{c as o}from"./theme.e1e15d89.js";import{s as a,j as c}from"./index.486f7255.js";var d="/home/runner/work/ui-components/ui-components/src/components/Button.tsx";const u=a.button`
+  background-color: ${o.button.background};
+  color: ${o.palette.white};
+  font-size: 1.6rem;
+  font-weight: 700;
+  line-height: 2rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 4rem;
+  padding: 0 3rem;
+  border: 0;
+  border-radius: 5px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+
+  &:not([disabled]) {
+    cursor: pointer;
+    &:hover {
+      background: ${o.button.backgroundHover}
+    }
+    &:active {
+      background: ${o.button.backgroundActive}
+    }
+  }
+  &:disabled {
+    opacity: 0.4;
+  }
+`,m=t=>{const{disabled:r,isWaiting:e,waitingText:n,children:i,...s}=t;return c(u,{...s,disabled:e||r,children:e&&n||i},void 0,!1,{fileName:d,lineNumber:47,columnNumber:5},void 0)};export{m as B};
