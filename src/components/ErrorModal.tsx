6275fea7a1a8f8c96caee1393994acee4210a0f6
tom <tom.g.woodward@gmail.com>
2023-02-03 14:51:27 -0500

    stub error display component

--- src/components/ErrorModal.tsx
+++ src/components/ErrorModal.tsx
@@ -1,15 +1,12 @@
 import { Button } from "./Button";
-import { Modal, ModalBody, ModalBodyHeading, ModalFooter, ModalPropTypes } from "./Modal";
+import { Modal, ModalFooter, ModalPropTypes } from "./Modal";
+import { Error } from "./Error";
 
 type ErrorModalProps = React.PropsWithChildren<Omit<ModalPropTypes, 'variant' | 'heading'>>;
 
 export const ErrorModal = (props: ErrorModalProps) => {
   return <Modal {...props} variant='error' heading='Error'>
-    <ModalBody>
-      <ModalBodyHeading>Uh-oh, there's been a glitch</ModalBodyHeading>
-      We're not quite sure what went wrong. Restart your browser. If this doesn't solve
-      the problem, visit our <a href="https://openstax.secure.force.com/help" target="_blank">Support Center</a>.
-    </ModalBody>
+    <Error />
     <ModalFooter><Button onClick={props.onModalClose}>OK</Button></ModalFooter>
   </Modal>;
 };
