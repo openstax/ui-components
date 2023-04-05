9b34cb20733153df34f76f44f4346049df340169
staxly <staxly@users.noreply.github.com>
2023-04-05 21:09:41 +0000

    building refs/heads/text-components/87508dc8068e04646ca394b2c70d5e967d32c3f8

---
+++ refs/heads/text-components/87508dc8068e04646ca394b2c70d5e967d32c3f8/assets/Text.stories.6b3eddfe.js
@@ -0,0 +1,8 @@
+import{s as o,j as e,F as t}from"./index.85753e9f.js";import{p as n}from"./palette.15e4d008.js";const i=o.h2`
+  color: ${n.neutralDarker}  
+  size: 36px;
+  font-weight: 700;
+`,a=o.p`
+  color: ${n.neutralDarker};
+  size: '18px';
+`;var r="/home/runner/work/ui-components/ui-components/src/components/Text.stories.tsx";const l=()=>e(t,{children:e(a,{children:"Body text"},void 0,!1,{fileName:r,lineNumber:4,columnNumber:3},void 0)},void 0,!1),m=()=>e(t,{children:e(i,{children:"Heading text"},void 0,!1,{fileName:r,lineNumber:8,columnNumber:3},void 0)},void 0,!1);typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{m as Heading,l as PText};
