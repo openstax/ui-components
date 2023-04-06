6569c9ac1b7821124775ea809f8df2ff4360ef42
Beth Shook <es66@rice.edu>
2023-04-06 09:39:05 -0500

    font size

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -3,11 +3,11 @@ import { palette } from "../theme/palette";
 
 export const H2 = styled.h2`
   color: ${palette.neutralDarker}  
-  size: 36px;
+  font-size: 3.6rem;
   font-weight: 700;
 `;
 
 export const Paragraph = styled.p`
   color: ${palette.neutralDarker};
-  size: '18px';
+  font-size: '1.8rem';
 `;
