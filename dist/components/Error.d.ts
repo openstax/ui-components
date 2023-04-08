aa3158224ea83ae521bb7136e0496722b58553f5
Beth Shook <es66@rice.edu>
2023-04-07 20:12:06 -0500

    dist

---
+++ dist/components/Error.d.ts
@@ -0,0 +1,6 @@
+export interface ErrorPropTypes {
+    className?: string;
+    children?: React.ReactNode;
+    heading?: string;
+}
+export declare const Error: ({ heading, children, ...props }: ErrorPropTypes) => JSX.Element;
---
+++ dist/components/Modal.d.ts
@@ -0,0 +1,11 @@
+export declare const ModalBodyHeading: import("styled-components").StyledComponent<"h3", any, {}, never>;
+export declare const ModalBody: import("styled-components").StyledComponent<"div", any, {}, never>;
+export declare const ModalFooter: import("styled-components").StyledComponent<"div", any, {}, never>;
+export interface ModalPropTypes {
+    onModalClose: () => void;
+    heading: string;
+    className?: string;
+    show?: boolean;
+    variant?: 'default' | 'error';
+}
+export declare const Modal: ({ className, heading, onModalClose, children, show, variant }: React.PropsWithChildren<ModalPropTypes>) => JSX.Element | null;
