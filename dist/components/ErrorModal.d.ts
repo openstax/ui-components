a679cd48b142f84a6e52bb3e7b09048403ce8526
Josiah Ivey <josiah.ivey@gmail.com>
2023-06-02 09:22:13 -0700

    1.3.2

---
+++ dist/components/ErrorModal.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+import { ModalPropTypes } from "./Modal";
+declare type ErrorModalProps = React.PropsWithChildren<Omit<ModalPropTypes, 'variant' | 'heading'>>;
+export declare const ErrorModal: (props: ErrorModalProps) => JSX.Element;
+export {};
---
+++ dist/components/Text.stories.d.ts
@@ -0,0 +1,2 @@
+export declare const PText: () => JSX.Element;
+export declare const Heading: () => JSX.Element;
