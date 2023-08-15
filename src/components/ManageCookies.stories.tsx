e75e9fd73d4acd3cdd35dda5fd23f5dbf18c3215
tom <tom.g.woodward@gmail.com>
2023-08-15 17:16:34 -0400

    Add test for color overrides

--- src/components/ManageCookies.stories.tsx
+++ src/components/ManageCookies.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from "styled-components";
 import { ManageCookiesLink } from "./ManageCookies";
 
 export const WithoutOsano = () => {
@@ -10,8 +11,18 @@ export const WithoutOsano = () => {
   return show ? <ManageCookiesLink /> : null;
 };
 
+const SomeContainer = styled.div`
+  a, button {
+    color: #ccc;
+
+    &:hover {
+      color: #000;
+    }
+  }
+`;
+
 export const WithOsano = () => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (window as any).Osano = {cm: {mode: 'production', showDrawer: (...args: any[]) => alert('showDrawer called ' + JSON.stringify(args))}};
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -19,5 +30,10 @@ export const WithOsano = () => {
 
   return <>
     <ManageCookiesLink />
+
+    <SomeContainer>
+      if there is some container like a footer or whatever that sets colors, it works
+      <ManageCookiesLink />
+    </SomeContainer>
   </>;
 };
