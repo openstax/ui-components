dd47868db8bc21e398147ef46d69e0adc06888cd
tom <tom.g.woodward@gmail.com>
2023-08-17 12:28:16 -0400

    test coverage

--- src/components/Button.spec.tsx
+++ src/components/Button.spec.tsx
@@ -1,4 +1,4 @@
-import { Button, LinkButton } from './Button';
+import { Button, LinkButton, PlainButton, ButtonLink } from './Button';
 import renderer from 'react-test-renderer';
 
 describe('Button', () => {
@@ -33,6 +33,20 @@ describe('Button', () => {
   it('renders as a tag variant', () => {
     const tree = renderer.create(
       <LinkButton variant='light'>Click Me</LinkButton>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders plain button', () => {
+    const tree = renderer.create(
+      <PlainButton>Click Me</PlainButton>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders button that looks like a link', () => {
+    const tree = renderer.create(
+      <ButtonLink>Click Me</ButtonLink>
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
