6c8775db7ccb4068d43164a62980cf9034d5b613
staxly <staxly@users.noreply.github.com>
2023-09-05 00:44:00 +0000

    building refs/heads/overlay/69f100eaf854abf716f948495aa8d35e32cd460e

---
+++ refs/heads/overlay/69f100eaf854abf716f948495aa8d35e32cd460e/assets/Error.551d466e.js
@@ -0,0 +1,5 @@
+import{R as a,s as l,j as s,a as o,F as c}from"./index.edb95f4e.js";import{M as m,a as p}from"./Modal.5111ad46.js";import{c as i}from"./theme.521bbb23.js";const u=a.createContext(null),d=l.div`
+  font-size: 0.9rem;
+  color: ${i.palette.neutralMedium};
+  margin-top: 1.2rem;
+`,v=({heading:t,children:e,...n})=>{const r=a.useContext(u);return s(m,{...n,"data-testid":"error",children:[o(p,{children:t!=null?t:"Uh-oh, there's been a glitch"}),e!=null?e:s(c,{children:["We're not quite sure what went wrong. Restart your browser. If this doesn't solve the problem, visit our ",o("a",{href:"https://openstax.secure.force.com/help",target:"_blank",children:"Support Center"}),"."]}),o(d,{children:r==null?void 0:r.eventId})]})};export{v as E,u as a};
