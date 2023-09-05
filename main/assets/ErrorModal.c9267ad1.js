8cb99db46e4404e9571c1ab9641382ffb5d85e0c
staxly <staxly@users.noreply.github.com>
2023-09-05 03:27:07 +0000

    building main

---
+++ main/assets/ErrorModal.c9267ad1.js
@@ -0,0 +1,1 @@
+import{B as a}from"./Button.2c1d02e2.js";import{b as s,c as t}from"./Modal.95979fba.js";import{E as i}from"./Error.1e1ab74b.js";import{j as n,a as r}from"./index.fca9c500.js";const c=o=>n(s,{...o,variant:"error",heading:"Error",children:[r(i,{}),r(t,{children:r(a,{onClick:o.onModalClose,children:"OK"})})]});export{c as E};
