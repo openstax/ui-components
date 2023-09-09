72cf29d181449005939f406900aa984ca8c843b5
staxly <staxly@users.noreply.github.com>
2023-09-09 00:03:58 +0000

    building refs/tags/1.4.0/2ce49ba7c098333f434a5246842804a5d5a63f99

---
+++ refs/tags/1.4.0/2ce49ba7c098333f434a5246842804a5d5a63f99/assets/ErrorModal.595a8f2a.js
@@ -0,0 +1,1 @@
+import{B as a}from"./Button.1ec47638.js";import{b as s,c as t}from"./Modal.da4ccd30.js";import{E as i}from"./Error.d99c48d2.js";import{j as n,a as r}from"./index.72fa4726.js";const c=o=>n(s,{...o,variant:"error",heading:"Error",children:[r(i,{}),r(t,{children:r(a,{onClick:o.onModalClose,children:"OK"})})]});export{c as E};
