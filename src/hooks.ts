cc7a383b9f2285d01e3a71255fe65bf2b803046e
Josiah Ivey <josiah.ivey@gmail.com>
2023-10-16 07:55:12 -0700

    add comment describing hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,11 +2,14 @@ import React from 'react';
 import { ErrorContext } from './contexts';
 import { getTypeFromError } from './utils';
 
+// Convenience hook for manually displaying an app-level error screen.
+// Takes a plain error object, or null to clear the error.
 export const useSetAppError = () => {
   const { setError } = React.useContext(ErrorContext);
 
   return React.useCallback((error: Error | null) => {
     setError(
+      // ErrorBoundary expects a wrapped SentryError
       error ? {
         error: error,
         type: getTypeFromError(error),
