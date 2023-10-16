88f2f7d4f7c658f855125bcec2bdf35973ae030e
Josiah Ivey <josiah.ivey@gmail.com>
2023-10-16 12:25:01 -0700

    notify sentry within the hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,13 +1,17 @@
 import React from 'react';
 import { ErrorContext } from './contexts';
 import { getTypeFromError } from './utils';
+import * as Sentry from '@sentry/react';
 
-// Convenience hook for manually displaying an app-level error screen.
+// Convenience hook for manually displaying an app-level error screen and notifying Sentry.
 // Takes a plain error object, or null to clear the error.
 export const useSetAppError = () => {
   const { setError } = React.useContext(ErrorContext);
 
   return React.useCallback((error: Error | null) => {
+    if (error) {
+      Sentry.captureException(error);
+    }
     setError(
       // ErrorBoundary expects a wrapped SentryError
       error ? {
