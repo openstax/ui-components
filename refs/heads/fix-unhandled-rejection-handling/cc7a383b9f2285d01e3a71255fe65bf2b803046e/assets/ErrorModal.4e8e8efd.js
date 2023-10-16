c9ece60c8b5e9cd49a0741178b82380bfe3d416c
staxly <staxly@users.noreply.github.com>
2023-10-16 14:56:24 +0000

    building refs/heads/fix-unhandled-rejection-handling/cc7a383b9f2285d01e3a71255fe65bf2b803046e

---
+++ refs/heads/fix-unhandled-rejection-handling/cc7a383b9f2285d01e3a71255fe65bf2b803046e/assets/ErrorModal.4e8e8efd.js
@@ -0,0 +1,1 @@
+import{B as a}from"./Button.5b2d203c.js";import{b as s,c as t}from"./Modal.d806c093.js";import{E as i}from"./Error.0aa5589b.js";import{j as n,a as r}from"./index.7eacc9fc.js";const c=o=>n(s,{...o,variant:"error",heading:"Error",children:[r(i,{}),r(t,{children:r(a,{onClick:o.onModalClose,children:"OK"})})]});export{c as E};
