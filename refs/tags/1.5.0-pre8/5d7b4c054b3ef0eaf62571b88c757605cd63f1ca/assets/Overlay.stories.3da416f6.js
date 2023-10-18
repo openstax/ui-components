12466c11a5d5a998f89243493737d63afc6fc3e3
staxly <staxly@users.noreply.github.com>
2023-10-18 18:22:57 +0000

    building refs/tags/1.5.0-pre8/5d7b4c054b3ef0eaf62571b88c757605cd63f1ca

---
+++ refs/tags/1.5.0-pre8/5d7b4c054b3ef0eaf62571b88c757605cd63f1ca/assets/Overlay.stories.3da416f6.js
@@ -0,0 +1,21 @@
+import{s as t,j as n,a as e,r as i}from"./index.ce0b705b.js";import{d as l,C as d,e as c}from"./Modal.46a435da.js";import"./theme.176ac5ca.js";import"./palette.15e4d008.js";const u=t(l)`
+  background-color: rgba(0, 0, 0, 0.89);
+`,m=t(d)`
+  height: 4rem;
+  width: 4rem;
+  position: absolute;
+  right: 0;
+  top: 0;
+`,p=t(c)`
+  color: #fff;
+  padding: 4rem;
+`,f=t.div`
+  position: relative;
+  flex-grow: 1;
+  height: 100%;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`,h=({className:o,onClose:r,children:a,show:s})=>s?n(p,{className:o,children:[n(f,{children:[e(m,{onClick:r,variant:"inverted-circle"}),a]}),e(u,{})]}):null,x=()=>{const[o,r]=i.exports.useState(!0);return e(h,{onClose:()=>r(!1),show:o,children:e("h1",{children:"Overlay"})})};typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{x as Default};
