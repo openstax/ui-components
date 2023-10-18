a0977adc81f1e18638119188242ad03ff973fd6c
Dante Soares <dante.m.soares@rice.edu>
2023-10-18 13:48:41 -0300

    Allow ErrorModal to set Error props

--- src/components/ErrorModal.tsx
+++ src/components/ErrorModal.tsx
@@ -2,11 +2,15 @@ import { Button } from "./Button";
 import { Modal, ModalFooter, ModalPropTypes } from "./Modal";
 import { Error } from "./Error";
 
-type ErrorModalProps = React.PropsWithChildren<Omit<ModalPropTypes, 'variant' | 'heading'>>;
+type ErrorModalProps = React.PropsWithChildren<Omit<ModalPropTypes, 'variant'>>;
 
 export const ErrorModal = (props: ErrorModalProps) => {
-  return <Modal {...props} variant='error' heading='Error'>
-    <Error />
+  const { children, heading, ...modalProps } = props;
+
+  return <Modal {...modalProps} variant='error' heading='Error'>
+    <Error heading={heading}>
+      {children}
+    </Error>
     <ModalFooter><Button onClick={props.onModalClose}>OK</Button></ModalFooter>
   </Modal>;
 };
