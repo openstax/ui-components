d301d9a4ea278f2a48f856110e0c68d32fa17fa9
Dante Soares <dante.m.soares@rice.edu>
2023-10-18 14:42:23 -0300

    Added snapshot test for ErrorModal with heading and children

--- src/components/ErrorModal.spec.tsx
+++ src/components/ErrorModal.spec.tsx
@@ -8,4 +8,14 @@ describe('Modal', () => {
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+  it('matches snapshot when heading and children are set', () => {
+    const tree = renderer.create(
+      <ErrorModal heading='Scores not sent' onModalClose={jest.fn()} show={true}>
+        There was an issue syncing your scores. Please try again in a few minutes. If the issue persists, visit our
+        <a href="https://openstax.secure.force.com/help" target="_blank" rel="noreferrer">Support Center</a>.
+      </ErrorModal>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
