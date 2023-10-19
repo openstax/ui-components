6cc88c42f5bf9aeee40e46c305aed8d85623917b
Josiah Ivey <josiah.ivey@gmail.com>
2023-10-19 11:36:50 -0700

    did not need a rename

--- .ladle/config.mjs
+++ .ladle/config.mjs
@@ -2,5 +2,5 @@ export default {
   base: "/ui-components/",
   outDir: "docs",
   mode: "preview",
-  viteConfig: process.cwd() + "/.ladle/ladle-vite.config.ts"
+  viteConfig: process.cwd() + "/.ladle/vite.config.ts"
 };
