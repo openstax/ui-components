ae92388e382f12de3c753041e519736eda556d68
staxly <staxly@users.noreply.github.com>
2024-01-29 20:59:34 +0000

    building refs/heads/headings/0bbe7d110f3b7819ec26cf8febf7625b14cb0864

---
+++ refs/heads/headings/0bbe7d110f3b7819ec26cf8febf7625b14cb0864/assets/Text.stories.8fa0ee0f.js
@@ -0,0 +1,19 @@
+import{s as t,a as e,F as r,j as o}from"./index.13f6b84d.js";import{p as n}from"./palette.15e4d008.js";const i=t.h2`
+  color: ${n.neutralDarker};
+  font-size: 3.6rem;
+  font-weight: 700;
+`,a=t.h3`
+  color: ${n.neutralDarker};
+  font-size: 1.4rem;
+  font-weight: 700;
+  line-height: 2.4rem;
+`,s=t.h4`
+  color: ${n.neutralThin};
+  font-size: 1.4rem;
+  font-weight: 700;
+  line-height: 2rem;
+  text-transform: uppercase;
+`,d=t.p`
+  color: ${n.neutralDarker};
+  font-size: 1.8rem;
+`,h=()=>e(r,{children:e(d,{children:"Body text"})}),m=()=>o(r,{children:[e(i,{children:"Heading text"}),e(a,{children:"Heading text"}),e(s,{children:"Heading text"})]});typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{m as Headings,h as PText};
