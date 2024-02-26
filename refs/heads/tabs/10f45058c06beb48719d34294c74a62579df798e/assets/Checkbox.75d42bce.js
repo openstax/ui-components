90b0af83250ae983ca469ef749889f7f25ab5f0d
staxly <staxly@users.noreply.github.com>
2024-02-26 18:52:19 +0000

    building refs/heads/tabs/10f45058c06beb48719d34294c74a62579df798e

---
+++ refs/heads/tabs/10f45058c06beb48719d34294c74a62579df798e/assets/Checkbox.75d42bce.js
@@ -0,0 +1,14 @@
+import{c as o}from"./theme.8c810d80.js";import{s as a,j as l,a as s}from"./index.06999ef1.js";const t={primary:{accentColor:o.palette.mediumBlue,boxShadow:"none"},light:{accentColor:o.palette.white,boxShadow:"0 0 1px 0"}},h=a.label`
+  font-size: 1.4rem;
+  display: flex;
+  align-items: center;
+  font-weight: ${e=>e.bold?700:400}
+`,x=a.input`
+  accent-color: ${e=>t[e.variant].accentColor};
+  width: ${e=>e.checkboxSize}rem;
+  height: ${e=>e.checkboxSize}rem;
+  margin: 0 1.6rem 0 0;
+  &:checked {
+    box-shadow: ${e=>t[e.variant].boxShadow};
+  }
+`,d=({children:e,variant:c="primary",bold:r=!1,size:i=1.6,...n})=>l(h,{bold:r,children:[s(x,{...n,type:"checkbox",variant:c,checkboxSize:i}),e]});export{d as C};
---
+++ refs/heads/tabs/10f45058c06beb48719d34294c74a62579df798e/assets/Text.stories.241e6d33.js
@@ -0,0 +1,8 @@
+import{s as t,a as e,F as n}from"./index.06999ef1.js";import{p as o}from"./palette.15e4d008.js";const r=t.h2`
+  color: ${o.neutralDarker};
+  font-size: 3.6rem;
+  font-weight: 700;
+`,a=t.p`
+  color: ${o.neutralDarker};
+  font-size: 1.8rem;
+`,c=()=>e(n,{children:e(a,{children:"Body text"})}),s=()=>e(n,{children:e(r,{children:"Heading text"})});typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{s as Heading,c as PText};
