1e4710443fcb5a7d166c962a4106e3c42ce1f211
Josiah Ivey <josiah.ivey@gmail.com>
2024-02-25 20:28:46 -0800

    add tabs component

---
+++ src/components/Tabs.spec.tsx
@@ -0,0 +1,72 @@
+import { render } from "@testing-library/react";
+import { Tabs, Tab, TabList, TabPanel } from "./Tabs";
+
+describe("Tabs component", () => {
+  describe("normal styling", () => {
+    it("renders correctly", () => {
+      const { asFragment } = render(
+        <Tabs>
+          <TabList aria-label="Items">
+            <Tab id="one">First Item</Tab>
+            <Tab id="two">Second Item</Tab>
+            <Tab id="three">Last Item</Tab>
+          </TabList>
+          <TabPanel id="one">First Content Panel</TabPanel>
+          <TabPanel id="two">Second Content Panel</TabPanel>
+          <TabPanel id="three">Third Content Panel</TabPanel>
+        </Tabs>,
+      );
+      expect(asFragment()).toMatchSnapshot();
+    });
+
+    it("renders correctly with small size", () => {
+      const { asFragment } = render(
+        <Tabs size="small">
+          <TabList aria-label="Items">
+            <Tab id="one">First Item</Tab>
+            <Tab id="two">Second Item</Tab>
+            <Tab id="three">Last Item</Tab>
+          </TabList>
+          <TabPanel id="one">First Content Panel</TabPanel>
+          <TabPanel id="two">Second Content Panel</TabPanel>
+          <TabPanel id="three">Third Content Panel</TabPanel>
+        </Tabs>,
+      );
+      expect(asFragment()).toMatchSnapshot();
+    });
+  });
+
+  describe("button-bar styling", () => {
+    it("renders correctly with button-bar variant", () => {
+      const { asFragment } = render(
+        <Tabs variant="button-bar">
+          <TabList aria-label="Items">
+            <Tab id="one">First Item</Tab>
+            <Tab id="two">Second Item</Tab>
+            <Tab id="three">Last Item</Tab>
+          </TabList>
+          <TabPanel id="one">First Content Panel</TabPanel>
+          <TabPanel id="two">Second Content Panel</TabPanel>
+          <TabPanel id="three">Third Content Panel</TabPanel>
+        </Tabs>,
+      );
+      expect(asFragment()).toMatchSnapshot();
+    });
+
+    it("renders correctly with button-bar variant and small size", () => {
+      const { asFragment } = render(
+        <Tabs size="small" variant="button-bar">
+          <TabList aria-label="Items">
+            <Tab id="one">First Item</Tab>
+            <Tab id="two">Second Item</Tab>
+            <Tab id="three">Last Item</Tab>
+          </TabList>
+          <TabPanel id="one">First Content Panel</TabPanel>
+          <TabPanel id="two">Second Content Panel</TabPanel>
+          <TabPanel id="three">Third Content Panel</TabPanel>
+        </Tabs>,
+      );
+      expect(asFragment()).toMatchSnapshot();
+    });
+  });
+});
