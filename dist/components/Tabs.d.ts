999c0ca552c1102a8a2b5c9023154b4cd0ca474c
Josiah Ivey <josiah.ivey@gmail.com>
2024-03-04 09:34:26 -0800

    dist

--- dist/components/Tabs.d.ts
+++ dist/components/Tabs.d.ts
@@ -1,10 +1,10 @@
 import React from "react";
-import * as AC from "react-aria-components";
+import * as RAC from "react-aria-components";
 declare type TabsProps = {
     variant?: "button-bar";
     size?: "large" | "medium" | "small";
     className?: string;
     children?: React.ReactNode;
-} & AC.TabsProps;
+} & RAC.TabsProps;
 export declare const Tabs: ({ variant, size, className, children, ...restProps }: TabsProps) => JSX.Element;
-export { TabList, Tab, TabPanel } from "react-aria-components";
+export { TabList, Tab, TabPanel, TabsContext, TabListStateContext } from "react-aria-components";
