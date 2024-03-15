bcf508292a374e64a073396e6da04b808c293632
Beth Shook <es66@rice.edu>
2024-03-15 12:57:10 -0500

    dist

---
+++ dist/components/NavBar.stories.d.ts
@@ -0,0 +1,5 @@
+export declare const Plain: () => JSX.Element;
+export declare const LogoAndChildren: () => JSX.Element;
+export declare const AltTextLinkedLogo: () => JSX.Element;
+export declare const AltTextNoLinkedLogo: () => JSX.Element;
+export declare const OverrideJustifyContent: () => JSX.Element;
