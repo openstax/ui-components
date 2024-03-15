10bdf7de7a565afbcc0a516f2607d8135c26b11b
staxly <staxly@users.noreply.github.com>
2024-03-15 19:32:56 +0000

    building refs/heads/dist-make-toast-aria-live/c6607e0b6b385cbba130b49c5efa4b6310f2ec34

---
+++ refs/heads/dist-make-toast-aria-live/c6607e0b6b385cbba130b49c5efa4b6310f2ec34/assets/buttons.3d122954.js
@@ -0,0 +1,14 @@
+import{C as c}from"./index.62f2402d.js";import{p as o}from"./palette.15e4d008.js";const a=t=>t,n=a({primary:{background:o.orange,backgroundActive:"#b03808",backgroundHover:"#be3c08",color:o.white},light:{background:o.white,backgroundActive:o.neutralLight,backgroundHover:o.white,color:o.neutralDarker,fontWeight:400},secondary:{background:o.darkGray,backgroundActive:"#4c4c4c",backgroundHover:"#646464",color:o.white}}),b=t=>{var e;const r=n[t];return c`
+    background-color: ${r.background};
+    color: ${r.color};
+    font-weight: ${(e=r.fontWeight)!=null?e:700};
+
+    &:not([disabled]) {
+      &:hover {
+        background: ${r.backgroundHover};
+      }
+      &:active {
+        background: ${r.backgroundActive};
+      }
+    }
+  `};export{b as a};
