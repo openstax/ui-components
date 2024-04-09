b26adf2d41c26e3d1baccf86e696e0378ee56ffc
staxly <staxly@users.noreply.github.com>
2024-04-09 22:15:55 +0000

    building refs/heads/radio-styles/73158398448fc068764cbb4f499f48ccb7940925

---
+++ refs/heads/radio-styles/73158398448fc068764cbb4f499f48ccb7940925/assets/Radio.d4e1c0dd.js
@@ -0,0 +1,32 @@
+import{c as e}from"./theme.14a74ce1.js";import{s as r,j as a,a as i}from"./index.6315cb7f.js";const n=r.label`
+  font-size: 1.4rem;
+  display: flex;
+  align-items: center;
+`,s=r.input`
+  appearance: none;
+  /* For iOS < 15 to remove gradient background */
+  background-color: ${e.palette.white};
+  font: inherit;
+  color: ${e.palette.pale};
+  width: 2rem;
+  height: 2rem;
+  border: 1px solid currentColor;
+  border-radius: 50%;
+  transform: translateY(-0.075em);
+  margin: 0 1.6rem 0 0;
+  display: grid;
+  place-content: center;
+
+  &::before {
+    content: "";
+    width: 1.6rem;
+    height: 1.6rem;
+    border-radius: 50%;
+    opacity: 0;
+    box-shadow: inset 1em 1em ${e.palette.mediumBlue};
+  }
+
+  &:checked::before {
+    opacity: 1;
+  }
+`,c=({children:t,...o})=>a(n,{children:[i(s,{...o,type:"radio"}),t]});export{c as R};
---
+++ refs/heads/radio-styles/73158398448fc068764cbb4f499f48ccb7940925/assets/Radio.stories.67e06d6b.js
@@ -0,0 +1,8 @@
+import{s as n,a as e,F as o,j as a}from"./index.6315cb7f.js";import{R as d}from"./Radio.d4e1c0dd.js";import"./theme.14a74ce1.js";import"./palette.15e4d008.js";const r=n.div`
+  & + & {
+    margin-top: 3.2rem;
+  }
+  > * + * {
+    margin-top: 0.5rem;
+  }
+`,i=t=>a(r,{children:[e(d,{...t,children:"Label 1"}),e(d,{...t,defaultChecked:!0,children:"Label 2"}),e(d,{...t,children:"Label 3"})]}),u=()=>e(o,{children:i({name:"default"})});typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{u as Default};
