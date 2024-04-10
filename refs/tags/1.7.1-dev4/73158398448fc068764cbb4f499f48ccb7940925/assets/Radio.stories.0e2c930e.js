1cd605f90dd3fd3d1b9d7167141e60c0831c024d
staxly <staxly@users.noreply.github.com>
2024-04-10 19:47:12 +0000

    building refs/tags/1.7.1-dev4/73158398448fc068764cbb4f499f48ccb7940925

---
+++ refs/tags/1.7.1-dev4/73158398448fc068764cbb4f499f48ccb7940925/assets/Radio.stories.0e2c930e.js
@@ -0,0 +1,8 @@
+import{s as n,a as e,F as o,j as a}from"./index.e7c823cd.js";import{R as d}from"./Radio.16d4e333.js";import"./theme.c3b93582.js";import"./palette.15e4d008.js";const r=n.div`
+  & + & {
+    margin-top: 3.2rem;
+  }
+  > * + * {
+    margin-top: 0.5rem;
+  }
+`,i=t=>a(r,{children:[e(d,{...t,children:"Label 1"}),e(d,{...t,defaultChecked:!0,children:"Label 2"}),e(d,{...t,children:"Label 3"})]}),u=()=>e(o,{children:i({name:"default"})});typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{u as Default};
