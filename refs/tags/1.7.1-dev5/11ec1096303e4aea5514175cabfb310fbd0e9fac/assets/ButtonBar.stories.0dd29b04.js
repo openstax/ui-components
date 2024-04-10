3319d5342accd04342bc71b59fc89a8628d82e2b
staxly <staxly@users.noreply.github.com>
2024-04-10 20:26:31 +0000

    building refs/tags/1.7.1-dev5/11ec1096303e4aea5514175cabfb310fbd0e9fac

---
+++ refs/tags/1.7.1-dev5/11ec1096303e4aea5514175cabfb310fbd0e9fac/assets/ButtonBar.stories.0dd29b04.js
@@ -0,0 +1,12 @@
+import{s as o,a as e,R as l,j as c}from"./index.c4aa4115.js";import{t as u,b as p,a as h,c as b}from"./Tabs.e7f6ead8.js";import"./theme.39a5a541.js";import"./palette.15e4d008.js";const S=o.div`
+  ${u}
+  ${p}
+
+  > * {
+    all: unset;
+    ${h}
+    ${b}
+  }
+`,i=({size:n="medium",children:r,...s})=>e(S,{size:n,...s,children:r}),C=o.div`
+  padding: 2.4rem;
+`,z=()=>{const[n,r]=l.useState("medium"),[s,m]=l.useState([0,2,null]),d=(t,a)=>{m([...s.slice(0,t),a,...s.slice(t+1)])};return c(C,{children:[c("label",{children:[e("b",{children:"Size"}),e("br",{}),e("select",{onChange:t=>r(t.currentTarget.value),defaultValue:n,children:["large","medium","small"].map(t=>e("option",{children:t},t))})]}),e("br",{}),e("br",{}),e(i,{size:n,children:["First Item","Second Item"].map((t,a)=>e("a",{"data-selected":s[0]===a,onClick:()=>d(0,a),children:t}))}),e("br",{}),e("br",{}),e(i,{size:n,children:["First Item","Second Item","Third Item"].map((t,a)=>e("a",{"data-selected":s[1]===a,onClick:()=>d(1,a),children:t}))}),e("br",{}),e("br",{}),e(i,{size:n,children:["First Item with a really long title","Second Item","Third Item"].map((t,a)=>e("a",{"data-selected":s[2]===a,onClick:()=>d(2,a),children:t}))})]})};typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{z as Examples};
---
+++ refs/tags/1.7.1-dev5/11ec1096303e4aea5514175cabfb310fbd0e9fac/assets/Radio.stories.4453e77a.js
@@ -0,0 +1,8 @@
+import{s as n,a as e,F as o,j as a}from"./index.c4aa4115.js";import{R as d}from"./Radio.b87fcf3b.js";import"./theme.39a5a541.js";import"./palette.15e4d008.js";const r=n.div`
+  & + & {
+    margin-top: 3.2rem;
+  }
+  > * + * {
+    margin-top: 0.5rem;
+  }
+`,i=t=>a(r,{children:[e(d,{...t,children:"Label 1"}),e(d,{...t,defaultChecked:!0,children:"Label 2"}),e(d,{...t,children:"Label 3"})]}),u=()=>e(o,{children:i({name:"default"})});typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{u as Default};
