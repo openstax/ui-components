5241fa1eec4ae55b70e3525e1b093d385b5dfd6a
Beth Shook <es66@rice.edu>
2024-04-24 16:01:01 -0500

    dist

---
+++ dist/components/ErrorBoundary.stories.d.ts
@@ -0,0 +1,4 @@
+export declare const InlineMessages: () => JSX.Element;
+export declare const Fallback_GenericError_Default: () => JSX.Element;
+export declare const Fallback_GenericError_Custom: () => JSX.Element;
+export declare const Fallback_SpecialError: () => JSX.Element;
