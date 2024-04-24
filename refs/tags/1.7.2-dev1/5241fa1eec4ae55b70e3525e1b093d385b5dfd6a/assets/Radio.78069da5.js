56e14797eb8e32e27cd75bf859c789a1fda3a81f
staxly <staxly@users.noreply.github.com>
2024-04-24 21:02:57 +0000

    building refs/tags/1.7.2-dev1/5241fa1eec4ae55b70e3525e1b093d385b5dfd6a

---
+++ refs/tags/1.7.2-dev1/5241fa1eec4ae55b70e3525e1b093d385b5dfd6a/assets/Radio.78069da5.js
@@ -0,0 +1,37 @@
+import{c as e}from"./theme.79b85ac5.js";import{s as o,j as a,a as i}from"./index.7f496473.js";const l=o.label`
+  font-size: 1.4rem;
+  display: flex;
+  align-items: center;
+  color: ${t=>t.disabled?e.palette.neutral:"inherit"}
+`,d=o.input`
+  appearance: none;
+  /* For iOS < 15 to remove gradient background */
+  background-color: ${e.palette.white};
+  font: inherit;
+  color: ${e.palette.pale};
+  width: 2rem;
+  height: 2rem;
+  border: 1px solid currentColor;
+  border-radius: 50%;
+  transform: translateY(-0.075em);
+  margin: 0 1.6rem 0 0;
+  display: grid;
+  place-content: center;
+
+  &:disabled {
+    background-color: ${e.palette.neutralLightest};
+  }
+
+  &::before {
+    content: "";
+    width: 1.6rem;
+    height: 1.6rem;
+    border-radius: 50%;
+    opacity: 0;
+    box-shadow: inset 1em 1em ${e.palette.mediumBlue};
+  }
+
+  &:checked::before {
+    opacity: 1;
+  }
+`,c=({children:t,...r})=>a(l,{disabled:r.disabled,children:[i(d,{...r,type:"radio"}),t]});export{c as R};
