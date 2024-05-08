722a490183ec9adee6e35b3db5fc20c3221a6a14
staxly <staxly@users.noreply.github.com>
2024-05-08 03:51:48 +0000

    building refs/heads/update-checkbox-label-props/0f501e444150b45680cb2b9ecd5cee769b26036d

---
+++ refs/heads/update-checkbox-label-props/0f501e444150b45680cb2b9ecd5cee769b26036d/assets/Radio.stories.df339450.js
@@ -0,0 +1,8 @@
+import{s as a,a as e,F as n,j as i}from"./index.5ab94fb3.js";import{R as d}from"./Radio.05c0ff63.js";import"./theme.25b6cac9.js";import"./palette.15e4d008.js";const o=a.div`
+  & + & {
+    margin-top: 3.2rem;
+  }
+  > * + * {
+    margin-top: 0.5rem;
+  }
+`,r=t=>i(o,{children:[e(d,{...t,children:"Label"}),e(d,{...t,defaultChecked:!0,children:"Label"}),e(d,{...t,children:"Label"}),e(d,{disabled:!0,...t,children:"Disabled label"})]}),u=()=>e(n,{children:r({name:"default"})});typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{u as Default};
---
+++ refs/heads/update-checkbox-label-props/0f501e444150b45680cb2b9ecd5cee769b26036d/assets/Tabs.stories.7fab246e.js
@@ -0,0 +1,13 @@
+import{s as o,R as s,j as t,a as e}from"./index.5ab94fb3.js";import{$ as m,T as a,d as l,e as n}from"./Tabs.206828bf.js";import"./theme.25b6cac9.js";import"./palette.15e4d008.js";const d=o(m)`
+  margin: 1rem 0;
+  padding: 5rem;
+  display: flex;
+  align-items: center;
+  border: 0.1rem solid #d5d5d5;
+`,h=o.div`
+  padding: 2.4rem;
+
+  .react-aria-TabList {
+    margin-top: 4.8rem;
+  }
+`,w=()=>{const[r,c]=s.useState("medium");return t(h,{children:[t("label",{children:[e("b",{children:"Size"}),e("br",{}),e("select",{onChange:i=>c(i.currentTarget.value),defaultValue:r,children:["large","medium","small"].map(i=>e("option",{children:i},i))})]}),t(a,{size:r,children:[t(l,{"aria-label":"Items",children:[e(n,{id:"one",children:"First Item"}),e(n,{id:"two",children:"Second Item"}),e(n,{id:"three",children:"Last Item"})]}),e(d,{id:"one",children:"First Content Panel"}),e(d,{id:"two",children:"Second Content Panel"}),e(d,{id:"three",children:"Third Content Panel"})]}),e("br",{}),t(a,{variant:"button-bar",size:r,children:[t(l,{"aria-label":"Items",children:[e(n,{id:"one",children:"First Item"}),e(n,{id:"two",children:"Second Item"}),e(n,{id:"three",children:"Last Item"})]}),e(d,{id:"one",children:"First Content Panel"}),e(d,{id:"two",children:"Second Content Panel"}),e(d,{id:"three",children:"Third Content Panel"})]})]})};typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{w as Examples};
