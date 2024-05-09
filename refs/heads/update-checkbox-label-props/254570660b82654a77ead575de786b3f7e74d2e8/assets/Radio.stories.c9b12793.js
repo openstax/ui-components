38aa897b48648b2bac89c42379022c2411deadba
staxly <staxly@users.noreply.github.com>
2024-05-09 18:49:01 +0000

    building refs/heads/update-checkbox-label-props/254570660b82654a77ead575de786b3f7e74d2e8

---
+++ refs/heads/update-checkbox-label-props/254570660b82654a77ead575de786b3f7e74d2e8/assets/Radio.stories.c9b12793.js
@@ -0,0 +1,8 @@
+import{s as a,a as e,F as n,j as i}from"./index.86a287c8.js";import{R as d}from"./Radio.454f1ddb.js";import"./theme.9ff62946.js";import"./palette.15e4d008.js";const o=a.div`
+  & + & {
+    margin-top: 3.2rem;
+  }
+  > * + * {
+    margin-top: 0.5rem;
+  }
+`,r=t=>i(o,{children:[e(d,{...t,children:"Label"}),e(d,{...t,defaultChecked:!0,children:"Label"}),e(d,{...t,children:"Label"}),e(d,{disabled:!0,...t,children:"Disabled label"})]}),u=()=>e(n,{children:r({name:"default"})});typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{u as Default};
