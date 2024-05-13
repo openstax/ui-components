1de700106ccf2fb0f62cb1dea884526fa12a04bd
staxly <staxly@users.noreply.github.com>
2024-05-13 21:14:14 +0000

    building refs/tags/1.7.6/e910d3e8e6b7f9627368a32d6e49bae266fb54c1

---
+++ refs/tags/1.7.6/e910d3e8e6b7f9627368a32d6e49bae266fb54c1/assets/Tooltip.stories.e37a2eb0.js
@@ -0,0 +1,17 @@
+import{s as e,a as o,j as i}from"./index.cf9a9aae.js";import{T as r}from"./Tooltip.6f6de10f.js";import"./theme.9cc20ba5.js";import"./palette.15e4d008.js";import"./module.894183c5.js";const n=e.div`
+  width: 50%;
+  margin: 0 auto;
+  font-size: 1.6rem;
+  position: relative;
+
+  > * {
+    margin-bottom: 1rem;
+  }
+`,m=e.div`
+  display: flex;
+  align-items: center;
+
+  > * {
+    margin-right: 1rem;
+  }
+`,c=()=>o(n,{children:["right","top","bottom"].map(t=>i(m,{children:[o(r,{placement:t,children:"Tooltip content goes here."}),t]}))});typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{c as Default};
