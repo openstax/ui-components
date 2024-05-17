74a4ce240d87c4b64530b7452f6f67b781f53353
Beth Shook <es66@rice.edu>
2024-05-17 14:27:02 -0500

    dist

---
+++ dist/components/Checkbox.stories.d.ts
@@ -0,0 +1,2 @@
+export declare const Primary: () => JSX.Element;
+export declare const Light: () => JSX.Element;
---
+++ dist/components/CloseModalButton.d.ts
@@ -0,0 +1,1 @@
+export declare const CloseModalButton: import("styled-components").StyledComponent<({ variant, ...props }: any) => JSX.Element, any, {}, never>;
