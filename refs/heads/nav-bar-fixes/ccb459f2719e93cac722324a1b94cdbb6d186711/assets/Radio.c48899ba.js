b66b9b1dd54a24868fcc440ddcbbbf40314e5864
staxly <staxly@users.noreply.github.com>
2024-05-24 17:48:18 +0000

    building refs/heads/nav-bar-fixes/ccb459f2719e93cac722324a1b94cdbb6d186711

---
+++ refs/heads/nav-bar-fixes/ccb459f2719e93cac722324a1b94cdbb6d186711/assets/Radio.c48899ba.js
@@ -0,0 +1,38 @@
+import{s,R as b,a as i,j as n}from"./index.c972267e.js";import{c as a}from"./theme.70755f8f.js";import{C as h}from"./Tooltip.39825d8a.js";import{a as u,b as f}from"./module.2fa82bb6.js";const c=s.label`
+  font-size: 1.4rem;
+  display: flex;
+  align-items: center;
+  color: ${e=>e.isDisabled?a.palette.pale:"inherit"};
+  width: 100%;
+  position: relative;
+`,d=s.input`
+  appearance: none;
+  /* For iOS < 15 to remove gradient background */
+  background-color: ${a.palette.white};
+  opacity: ${e=>e.isDisabled?"0.4":"1"};
+  font: inherit;
+  color: ${a.palette.pale};
+  width: 2rem;
+  height: 2rem;
+  border: 1px solid currentColor;
+  border-radius: 50%;
+  transform: translateY(-0.075em);
+  margin: 0 1.6rem 0 0;
+  display: grid;
+  place-content: center;
+
+  &::before {
+    content: "";
+    width: 1.6rem;
+    height: 1.6rem;
+    border-radius: 50%;
+    opacity: 0;
+    box-shadow: inset 1em 1em ${a.palette.mediumBlue};
+  }
+
+  &:checked::before {
+    opacity: ${e=>e.isDisabled?0:1};
+  }
+`,y=s.div`
+  display: inline-block;
+`,w=({children:e,disabled:t,...o})=>{const r=u({delay:0}),l=b.useRef(null),{triggerProps:p,tooltipProps:m}=f({delay:0},r,l);return o.tooltipText?i("div",{children:i(y,{children:n(c,{ref:l,isDisabled:t,"aria-disabled":t,...p,children:[i(d,{type:"radio",onFocus:()=>r.open(),isDisabled:t,"aria-disabled":t,...o}),e,r.isOpen&&i(h,{state:r,...m,placement:"right",children:o.tooltipText})]})})}):n(c,{isDisabled:t,"aria-disabled":t,children:[i(d,{type:"radio",isDisabled:t,"aria-disabled":t,...o}),e]})};export{w as R};
---
+++ refs/heads/nav-bar-fixes/ccb459f2719e93cac722324a1b94cdbb6d186711/assets/Radio.stories.42773c8e.js
@@ -0,0 +1,8 @@
+import{s as n,a as e,F as i,j as r}from"./index.c972267e.js";import{R as o}from"./Radio.c48899ba.js";import"./theme.70755f8f.js";import"./palette.15e4d008.js";import"./Tooltip.39825d8a.js";import"./module.2fa82bb6.js";const a=n.div`
+  & + & {
+    margin-top: 3.2rem;
+  }
+  > * + * {
+    margin-top: 0.5rem;
+  }
+`,d=t=>r(a,{children:[e(o,{...t,children:"Label"}),e(o,{...t,defaultChecked:!0,children:"Label"}),e(o,{...t,children:"Label"}),e(o,{disabled:!0,...t,children:"Disabled label"})]}),p=()=>e(i,{children:d({name:"default"})}),f=()=>e(i,{children:d({name:"withTooltip",tooltipText:"Tooltip text for radio input goes here"})});typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{p as Default,f as WithTooltip};
