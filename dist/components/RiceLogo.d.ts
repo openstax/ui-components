ddd4b7c2e0d24b409d9df1c3b9510bf93651bc2d
Josiah Ivey <josiah.ivey@gmail.com>
2024-06-10 09:42:23 -0700

    1.8.0-dev dist

---
+++ dist/components/RiceLogo.d.ts
@@ -0,0 +1,3 @@
+export declare const RiceLogo: ({ alt, ...props }: {
+    alt?: string | undefined;
+}) => JSX.Element;
---
+++ dist/components/forms/uncontrolled/inputTypes.d.ts
@@ -0,0 +1,53 @@
+import React from 'react';
+import { InputProps } from "./inputDecorations";
+import { AbstractFormData } from "../controlled/hooks";
+declare type TextInputProps = React.ComponentPropsWithoutRef<'input'> & InputProps & {
+    wrapperProps?: React.ComponentPropsWithoutRef<'label'>;
+    onChangeValue?: (value: any) => void;
+    transformValue?: (value: string | undefined) => any;
+    addon?: React.ReactElement;
+    pattern?: string;
+};
+export declare const TextInput: ({ label, addon, help, wrapperProps, onChangeValue, ...props }: TextInputProps) => JSX.Element;
+declare type TextAreaProps = React.ComponentPropsWithoutRef<'textarea'> & InputProps & {
+    wrapperProps?: React.ComponentPropsWithoutRef<'label'>;
+    onChangeValue?: (value: any) => void;
+    transformValue?: (value: string | undefined) => any;
+};
+export declare const TextArea: ({ label, help, wrapperProps, onChangeValue, ...props }: TextAreaProps) => JSX.Element;
+declare type SelectProps = React.ComponentPropsWithoutRef<'select'> & InputProps & {
+    options: Array<{
+        label: string;
+        value: string;
+        group?: string;
+    }>;
+    onChangeValue?: (value: any) => void;
+    transformValue?: (value: string | string[] | undefined) => any;
+    onOptionDisappear?: (disappeared: readonly string[]) => void;
+    onOptionReappear?: (reappeared: readonly string[]) => void;
+    onOptionAppear?: (appeared: readonly string[]) => void;
+    wrapperProps?: React.ComponentPropsWithoutRef<'label'>;
+    loading?: boolean;
+};
+export declare const Select: ({ onChangeValue, onOptionDisappear, onOptionAppear, onOptionReappear, label, help, options, wrapperProps, ...props }: SelectProps) => JSX.Element;
+declare type RadioProps = React.ComponentPropsWithoutRef<'input'> & InputProps & {
+    onChangeValue?: (value: boolean | undefined) => void;
+    wrapperProps?: React.ComponentPropsWithoutRef<'label'>;
+    tooltipText?: string;
+};
+export declare const Radio: ({ label, help, wrapperProps, onChangeValue, ...props }: RadioProps) => JSX.Element;
+declare type CheckboxProps = React.ComponentPropsWithoutRef<'input'> & InputProps & {
+    onChangeValue?: (value: boolean | undefined) => void;
+    wrapperProps?: React.ComponentPropsWithoutRef<'label'>;
+};
+export declare const Checkbox: ({ label, help, wrapperProps, onChangeValue, ...props }: CheckboxProps) => JSX.Element;
+declare type FileProps = React.ComponentPropsWithoutRef<'input'> & InputProps & {
+    accept?: string;
+    wrapperProps?: React.ComponentPropsWithoutRef<'label'>;
+    onChangeValue?: (value: any) => void;
+    uploader?: (value: FileList | null) => Promise<undefined | string | string[] | AbstractFormData | AbstractFormData[]>;
+    directory?: string;
+    webkitdirectory?: string;
+};
+export declare const File: ({ label, help, wrapperProps, onChangeValue, uploader, value, ...props }: FileProps) => JSX.Element;
+export {};
