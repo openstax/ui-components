54a3fc9073a5e9c8a2c0afb5c72b79abb37f9d2e
staxly <staxly@users.noreply.github.com>
2024-06-10 16:47:20 +0000

    building refs/heads/menu-component/66a4baef4d256216804ab3cf715aa65c4c0057e0

---
+++ refs/heads/menu-component/66a4baef4d256216804ab3cf715aa65c4c0057e0/assets/Tabs.stories.e8411c5d.js
@@ -0,0 +1,13 @@
+import{s as o,R as s,j as t,a as e}from"./index.7cd561cc.js";import{T as a}from"./Tabs.7d052152.js";import{m,n as l,o as n}from"./module.7b9fabff.js";import"./theme.576697f0.js";import"./palette.15e4d008.js";const d=o(m)`
+  margin: 1rem 0;
+  padding: 5rem;
+  display: flex;
+  align-items: center;
+  border: 0.1rem solid #d5d5d5;
+`,h=o.div`
+  padding: 2.4rem;
+
+  .react-aria-TabList {
+    margin-top: 4.8rem;
+  }
+`,$=()=>{const[i,c]=s.useState("medium");return t(h,{children:[t("label",{children:[e("b",{children:"Size"}),e("br",{}),e("select",{onChange:r=>c(r.currentTarget.value),defaultValue:i,children:["large","medium","small"].map(r=>e("option",{children:r},r))})]}),t(a,{size:i,children:[t(l,{"aria-label":"Items",children:[e(n,{id:"one",children:"First Item"}),e(n,{id:"two",children:"Second Item"}),e(n,{id:"three",children:"Last Item"})]}),e(d,{id:"one",children:"First Content Panel"}),e(d,{id:"two",children:"Second Content Panel"}),e(d,{id:"three",children:"Third Content Panel"})]}),e("br",{}),t(a,{variant:"button-bar",size:i,children:[t(l,{"aria-label":"Items",children:[e(n,{id:"one",children:"First Item"}),e(n,{id:"two",children:"Second Item"}),e(n,{id:"three",children:"Last Item"})]}),e(d,{id:"one",children:"First Content Panel"}),e(d,{id:"two",children:"Second Content Panel"}),e(d,{id:"three",children:"Third Content Panel"})]})]})};typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{$ as Examples};
