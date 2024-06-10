a1f89c27627a71ec66def4d77a22b89bfba4605e
Josiah Ivey <josiah.ivey@gmail.com>
2024-06-10 10:27:28 -0700

    1.8.0-dev5 dist

--- dist/components/Menu.d.ts
+++ dist/components/Menu.d.ts
@@ -1,8 +1,10 @@
 import React from 'react';
 import { ButtonProps, PopoverProps } from 'react-aria-components';
+export declare const MenuButton: import("styled-components").StyledComponent<(props: ButtonProps & React.RefAttributes<HTMLButtonElement>) => React.ReactElement<any, string | React.JSXElementConstructor<any>>, any, {}, never>;
+export declare const MenuPopover: import("styled-components").StyledComponent<(props: PopoverProps & React.RefAttributes<HTMLElement>) => React.ReactElement<any, string | React.JSXElementConstructor<any>>, any, {}, never>;
 export interface MenuProps {
     buttonLabel: string;
-    buttonIcon: string | React.ReactNode;
+    buttonIcon?: string | React.ReactNode;
     buttonProps?: ButtonProps;
     popoverProps?: PopoverProps;
 }
