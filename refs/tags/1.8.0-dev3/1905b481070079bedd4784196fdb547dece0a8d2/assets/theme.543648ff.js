05772c443070acd3b909f08d714026f96f35c380
staxly <staxly@users.noreply.github.com>
2024-06-10 16:52:40 +0000

    building refs/tags/1.8.0-dev3/1905b481070079bedd4784196fdb547dece0a8d2

---
+++ refs/tags/1.8.0-dev3/1905b481070079bedd4784196fdb547dece0a8d2/assets/theme.543648ff.js
@@ -0,0 +1,9 @@
+import{C as o}from"./index.ea1552af.js";import{p as s}from"./palette.15e4d008.js";const n={palette:s,link:{color:"#027EB5",hover:"#0064A0"}},m=["navbar","modals","toasts"].reduce((e,t,a)=>(e[t]=(a+1)*10,e),{}),r={navbar:{desktop:3.2,mobile:1.6}},d=50,i=75,c=i+.0625,p={mobileMedium:e=>o`
+      @media screen and (max-width: ${d}em) {
+        ${e}
+      }
+    `,desktop:e=>o`
+    @media screen and (min-width: ${c}em) {
+      ${e}
+    }
+  `},b={colors:n,zIndex:m,padding:r,breakpoints:p};export{n as c,b as t,m as z};
