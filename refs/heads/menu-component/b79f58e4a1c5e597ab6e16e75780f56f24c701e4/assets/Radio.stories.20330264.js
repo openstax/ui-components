333f164fd74ec22249c2f97c89f99490916aa7cf
staxly <staxly@users.noreply.github.com>
2024-06-10 17:04:43 +0000

    building refs/heads/menu-component/b79f58e4a1c5e597ab6e16e75780f56f24c701e4

---
+++ refs/heads/menu-component/b79f58e4a1c5e597ab6e16e75780f56f24c701e4/assets/Radio.stories.20330264.js
@@ -0,0 +1,8 @@
+import{s as r,a as e,F as i,j as n}from"./index.aeb47f24.js";import{R as o}from"./Radio.9236dcab.js";import"./theme.c1ff4564.js";import"./palette.15e4d008.js";import"./Tooltip.4194aa97.js";import"./module.a1f5f9c7.js";import"./Info.8c371020.js";const a=r.div`
+  & + & {
+    margin-top: 3.2rem;
+  }
+  > * + * {
+    margin-top: 0.5rem;
+  }
+`,d=t=>n(a,{children:[e(o,{...t,children:"Label"}),e(o,{...t,defaultChecked:!0,children:"Label"}),e(o,{...t,children:"Label"}),e(o,{disabled:!0,...t,children:"Disabled label"})]}),f=()=>e(i,{children:d({name:"default"})}),b=()=>e(i,{children:d({name:"withTooltip",tooltipText:"Tooltip text for radio input goes here"})});typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{f as Default,b as WithTooltip};
