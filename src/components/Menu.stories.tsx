fbe6c50f45cdf08b71a338d203697a1469a796bf
Josiah Ivey <josiah.ivey@gmail.com>
2024-06-11 13:56:37 -0700

    fix passing menu component into popover

--- src/components/Menu.stories.tsx
+++ src/components/Menu.stories.tsx
@@ -1,18 +1,72 @@
-import { MenuItem } from "react-aria-components";
-import { Menu } from "./Menu";
-import { Info } from './svgs/Info';
+import { Tab, TabList, TabPanel } from "react-aria-components";
+import { colors } from "../../src/theme";
+import styled from "styled-components";
+import { MenuButton, MenuItem, Menu, MenuGenericContainer } from "./Menu";
+import { NavBar } from "./NavBar";
+import { Tabs } from "./Tabs";
+
+const infoSvg = () => (
+  <svg width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M6 0.180664C2.68652 0.180664 0 2.86816 0 6.18066C0 9.49511 2.68652 12.1807 6 12.1807C9.31348 12.1807 12 9.49511 12 6.18066C12 2.86816 9.31348 0.180664 6 0.180664ZM6 2.84195C6.56119 2.84195 7.01613 3.29689 7.01613 3.85808C7.01613 4.41928 6.56119 4.87421 6 4.87421C5.43881 4.87421 4.98387 4.41928 4.98387 3.85808C4.98387 3.29689 5.43881 2.84195 6 2.84195ZM7.35484 8.98712C7.35484 9.14745 7.22485 9.27744 7.06452 9.27744H4.93548C4.77515 9.27744 4.64516 9.14745 4.64516 8.98712V8.40647C4.64516 8.24614 4.77515 8.11615 4.93548 8.11615H5.22581V6.56776H4.93548C4.77515 6.56776 4.64516 6.43777 4.64516 6.27744V5.69679C4.64516 5.53646 4.77515 5.40647 4.93548 5.40647H6.48387C6.6442 5.40647 6.77419 5.53646 6.77419 5.69679V8.11615H7.06452C7.22485 8.11615 7.35484 8.24614 7.35484 8.40647V8.98712Z" fill="#959595"/>
+  </svg>
+);
+
+const dotsBase64 = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAiIGhlaWdodD0iNTYiIHZpZXdCb3g9IjAgMCAxMCA1NiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICA8Y2lyY2xlIGN4PSI1IiBjeT0iNSIgcj0iNSIgZmlsbD0iIzAwMCIvPgogIDxjaXJjbGUgY3g9IjUiIGN5PSIyOCIgcj0iNSIgZmlsbD0iIzAwMCIvPgogIDxjaXJjbGUgY3g9IjUiIGN5PSI1MSIgcj0iNSIgZmlsbD0iIzAwMCIvPgo8L3N2Zz4K";
+
+const InfoMenuButton = styled(MenuButton)`
+  &:hover {
+    svg path {
+      fill: ${colors.palette.lightBlue};
+    }
+  }
+`;
+
+const DotsMenuButton = styled(MenuButton)`
+  padding: 1rem;
+`;
+
+const StyledMenuItem = styled(MenuItem)`
+  color: ${colors.palette.orange};
+`;
+
+const StyledWrapper = styled.div`
+  display: flex;
+  height: 100%
+`;
 
 export const Default = () =>
-  <>
-    <Menu buttonLabel="Menu">
-      <button>Example button</button>
-      <button>Another button</button>
-    </Menu>
-    <Menu buttonLabel="Test menu" buttonIcon={Info}>
-      <button>Example button</button>
-      <button>Another button</button>
-    </Menu>
-    <Menu buttonLabel="Test menu" buttonIcon={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAPoAAAD6AG1e1JrAAACh0lEQVR4nO2TT0gUURyAp1MUdPBUEBS7KpQakkJE2Rt2y3lvHXUlmBl3MCiK9ZDtzjxRO0SE/TPqkCBBIiHieyt7MAgjNSzNgnizIkFSkB62pejgTNQl8LIxawepNaeaXQj2g+/wTu97v/kNxxUoUCAL7SO1FTpFCVtM4bRO4UNMURwT2K9TdBsTdE0nqAtTeFYnwilMkKTFkIAJqtFGYJVGRG+E+rd3DDRu4/4GncAOTFHaPeEXTNFHncDF1YfBGUzQI52iluwBFHW7G5BdnaI36wTAC/kIwBTOZw0IP6jf2h6DJ3QKI5ig85jCHo2gPkzRvR+7MKFROIsJmsMULmGCPukUfv3TAI3AKS6pid5kRKx2y7muOvD8Yr0w2S02jl5FLfEbgTOxm4G2oVuws3MQtmKKzmUW2F7kEaGSS0XrVlLRunRejIjhX8adjAR630cD8VybigZiH7T6qt/+frIqH5ZV+ZhbKopSzDlFVVVvs9qcdtkXjgNCodAetwNkVTb+nwBJknYoIeWbmwGKqow5DrCRJGmLqqpFax3uO7473t/gmRiqrZimR/ez+8Kh+THB93pcQAvj/qa3kz558an/5NIT/+nkjF9LPvPpqVn+ku3yy5oei/E9FgN3TIO/axr8sMX4+GfGa9xGWAxctww+nQtNxr9yEjCQqwDLAAsbBpgMXPmpesVkvGUaYNk0+KWMDCRW5acsAzw2GT9qj9hkYDAzcsb32p/AZKDbNECXmQDtJgNhKwGOrHtxaaW4s7isKVxaHmw9cLBBq6huaLPP/6pnb3D9S9fiKQte9pYH027rKQ++45zgLWvsyFHAnKMAjuM27donFrltSQna7DSgQAEun3wHXrEVP1Vyp4cAAAAASUVORK5CYII="}>
-      <MenuItem>Cool menu item</MenuItem>
-    </Menu>
-  </>
+  <NavBar>
+    <MenuButton label="Menu">
+      <MenuGenericContainer>
+        <button>Example button</button>
+        <button>Another button</button>
+      </MenuGenericContainer>
+    </MenuButton>
+    <InfoMenuButton label="Test menu" icon={infoSvg}>
+      <Tabs>
+        <TabList aria-label="Items">
+          <Tab id="one">Info 1</Tab>
+          <Tab id="two">Info 2</Tab>
+          <Tab id="three">Info 3</Tab>
+        </TabList>
+        <TabPanel id="one">First</TabPanel>
+        <TabPanel id="two">Second</TabPanel>
+        <TabPanel id="three">Third</TabPanel>
+      </Tabs>
+    </InfoMenuButton>
+    <StyledWrapper>
+      <MenuButton label="Help">
+        <Menu>
+          <MenuItem>Open Guide</MenuItem>
+          <MenuItem>Contact Support</MenuItem>
+        </Menu>
+      </MenuButton>
+      <DotsMenuButton label="Test menu" icon={dotsBase64}>
+        <Menu>
+          <MenuItem>Cool menu item</MenuItem>
+          <MenuItem>Really long menu item with a lot of text</MenuItem>
+          <StyledMenuItem>Styled menu item</StyledMenuItem>
+        </Menu>
+      </DotsMenuButton>
+    </StyledWrapper>
+  </NavBar>
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,43 +1,98 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
-import { Button, ButtonProps, Dialog, DialogTrigger, Popover, PopoverProps } from 'react-aria-components';
+import {
+  Button,
+  ButtonProps,
+  Dialog,
+  DialogTrigger,
+  Menu as AriaMenu,
+  MenuItem as AriaMenuItem,
+  Popover,
+  PopoverProps,
+} from "react-aria-components";
+import { colors } from "../../src/theme";
 
-export const MenuButton = styled(Button)`
+export const Menu = styled(AriaMenu)``;
+export const MenuItem = styled(AriaMenuItem)``;
+
+const StyledMenuButton = styled(Button)`
   border: none;
   background: none;
   padding: 0;
   min-height: 4rem;
   min-width: 4rem;
+  height: 100%;
   display: inline-flex;
   place-content: center;
   align-items: center;
+  cursor: pointer;
+  font-weight: 500;
+
+  img {
+    max-height: 100%;
+  }
 `;
 
-export const MenuPopover = styled(Popover)`
+export const MenuGenericContainer = styled.div`
   padding: 1.6rem;
-  border-top: 0.4rem solid #63A524;
+`;
+
+const MenuPopover = styled(Popover)`
+  margin-top: -1rem;
+  border-top: 0.4rem solid ${colors.palette.darkGreen};
   box-shadow: 0 0.4rem 0.4rem 0 #00000033;
   background: #fff;
+
+  ${MenuItem} {
+    font-size: 1.4rem;
+    min-height: 4rem;
+    padding: 0 1.6rem;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+
+    &:hover {
+      background: ${colors.palette.neutralLighter};
+    }
+
+    &:active {
+      font-weight: bold;
+    }
+  }
 `;
 
 export interface MenuProps {
-  buttonLabel: string;
-  buttonIcon?: string | React.ReactNode;
+  label: string;
+  icon?: string | React.ReactNode;
   buttonProps?: ButtonProps;
   popoverProps?: PopoverProps;
+  className?: string;
 }
 
-export const Menu = ({ buttonLabel, buttonIcon, ...props }: React.PropsWithChildren<MenuProps>) => {
+export const MenuButton = ({
+  label,
+  icon,
+  ...props
+}: React.PropsWithChildren<MenuProps>) => {
   return (
     <DialogTrigger>
-      <MenuButton aria-label={buttonLabel} {...props.buttonProps}>
-        {buttonIcon ? (typeof buttonIcon === 'string' ?
-          <img aria-hidden='true' src={buttonIcon} alt='' /> : buttonIcon) : buttonLabel}
-      </MenuButton>
+      <StyledMenuButton
+        aria-label={label}
+        className={props.className}
+        {...props.buttonProps}
+      >
+        {icon ? (
+          typeof icon === "string" ? (
+            <img aria-hidden="true" src={icon} alt="" />
+          ) : (
+            icon
+          )
+        ) : (
+          label
+        )}
+      </StyledMenuButton>
       <MenuPopover {...props.popoverProps}>
-        <Dialog>
-          {props.children}
-        </Dialog>
+        <Dialog>{props.children}</Dialog>
       </MenuPopover>
     </DialogTrigger>
   );
