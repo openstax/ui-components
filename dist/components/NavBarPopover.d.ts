c32d75bd3c6d73343e9d4f8ffb34c7b5fd44e043
Josiah Ivey <josiah.ivey@gmail.com>
2024-06-12 17:02:02 -0700

    1.8.0-dev8 dist

---
+++ dist/components/NavBarPopover.d.ts
@@ -0,0 +1,23 @@
+import React from "react";
+import { PopoverProps } from "react-aria-components";
+import { NavBarButtonProps } from "./NavBarButton";
+export declare const NavBarMenuItem: import("styled-components").StyledComponent<(<T extends object>(props: import("react-aria-components").MenuItemProps<T> & React.RefAttributes<HTMLDivElement>) => React.ReactElement<any, string | React.JSXElementConstructor<any>>), any, {}, never>;
+export declare const PopoverContainer: import("styled-components").StyledComponent<"div", any, {}, never>;
+export declare const NavBarPopover: import("styled-components").StyledComponent<(props: PopoverProps & React.RefAttributes<HTMLElement>) => React.ReactElement<any, string | React.JSXElementConstructor<any>>, any, {}, never>;
+export declare type NavBarItemProps = React.PropsWithChildren<{
+    label?: string;
+    icon?: string | React.ReactNode;
+    buttonProps?: NavBarButtonProps;
+    popoverProps?: PopoverProps;
+    className?: string;
+}> & ({
+    label: string;
+    icon?: React.ReactNode;
+    "aria-label"?: string;
+} | {
+    label?: never;
+    icon: React.ReactNode;
+    "aria-label": string;
+});
+export declare const NavBarPopoverButton: (props: NavBarItemProps) => JSX.Element;
+export declare const NavBarMenuButton: (props: NavBarItemProps) => JSX.Element;
