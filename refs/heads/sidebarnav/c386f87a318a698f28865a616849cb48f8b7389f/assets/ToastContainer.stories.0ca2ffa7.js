901dddbe3988a274362d4b0fd51755d20d9781a1
staxly <staxly@users.noreply.github.com>
2024-08-09 02:40:19 +0000

    building refs/heads/sidebarnav/c386f87a318a698f28865a616849cb48f8b7389f

---
+++ refs/heads/sidebarnav/c386f87a318a698f28865a616849cb48f8b7389f/assets/ToastContainer.stories.0ca2ffa7.js
@@ -0,0 +1,15 @@
+import{s as a,C as m,a as t}from"./index.a26443a1.js";import{B as d}from"./BodyPortal.b1cb3288.js";import{T as l}from"./Toast.aae2db59.js";import{z as c}from"./theme.639ccfb1.js";import"./palette.15e4d008.js";const f=a(d)`
+  ${e=>!e.inline&&m`
+    position: fixed;
+    right: 2rem;
+  `}
+  z-index: ${c.toasts};
+  display: grid;
+  justify-items: center;
+  justify-content: center;
+  gap: 1vh;
+`,u=({toasts:e,onDismissToast:i,inline:s=!1,className:r})=>t(f,{inline:s,"aria-live":"polite",slot:"toast",className:r,children:e.map((o,n)=>t(l,{onDismiss:i,inline:s,...o,children:o.message},`toast-${n}`))}),p=a(u)`
+  top: 2rem;
+  left: 2rem;
+  right: unset;
+`,g=[{title:"Neutral",message:"message"},{title:"Error",message:"message",variant:"failure",dismissAfterMs:4e3},{title:"Success",message:"message",variant:"success",dismissAfterMs:2e3}],T=()=>t(p,{toasts:g});typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{T as Default};
