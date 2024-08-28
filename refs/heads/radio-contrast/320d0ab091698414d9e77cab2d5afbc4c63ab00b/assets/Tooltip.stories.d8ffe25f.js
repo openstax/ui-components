03dfe67dcc0e432ef326802bbe42f963cbe9887f
staxly <staxly@users.noreply.github.com>
2024-08-28 18:30:17 +0000

    building refs/heads/radio-contrast/320d0ab091698414d9e77cab2d5afbc4c63ab00b

---
+++ refs/heads/radio-contrast/320d0ab091698414d9e77cab2d5afbc4c63ab00b/assets/Tooltip.stories.d8ffe25f.js
@@ -0,0 +1,17 @@
+import{s as e,a as o,j as i}from"./index.ed164ab4.js";import{T as r}from"./Tooltip.5699bc21.js";import"./theme.aefcb3a1.js";import"./palette.15e4d008.js";import"./OverlayArrow.module.cd339e02.js";import"./focusSafely.module.bb24d709.js";import"./context.module.a3a1ea48.js";import"./Info.5759b1ad.js";const m=e.div`
+  width: 50%;
+  margin: 0 auto;
+  font-size: 1.6rem;
+  position: relative;
+
+  > * {
+    margin-bottom: 1rem;
+  }
+`,n=e.div`
+  display: flex;
+  align-items: center;
+
+  > * {
+    margin-right: 1rem;
+  }
+`,f=()=>o(m,{children:["right","top","bottom"].map(t=>i(n,{children:[o(r,{placement:t,children:"Tooltip content goes here."}),t]}))});typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{f as Default};
