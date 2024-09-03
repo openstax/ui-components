24056af54e7394be3780f0cc7b3007b41aa058c6
Josiah Ivey <josiah.ivey@gmail.com>
2024-09-03 10:31:27 -0700

    fix snapshots

--- src/components/SidebarNav.spec.tsx
+++ src/components/SidebarNav.spec.tsx
@@ -231,7 +231,7 @@ describe("SidebarNav", () => {
       expect(document.body).toMatchInlineSnapshot(`
 <body>
   <nav
-    class="sc-jSMfEi caMDhR"
+    class="sc-jSMfEi kqIBDA"
     data-portal-slot="sidebar"
     data-testid="sidebarnav"
   >
