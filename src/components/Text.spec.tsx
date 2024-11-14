bd74954101da7d44fb61efe32489f035a634eb92
Josiah Ivey <josiah.ivey@gmail.com>
2024-11-13 20:06:23 -0800

    add an h3 heading

--- src/components/Text.spec.tsx
+++ src/components/Text.spec.tsx
@@ -1,4 +1,4 @@
-import { Paragraph, H2 } from './Text';
+import { Paragraph, H2, H3 } from './Text';
 import renderer from 'react-test-renderer';
 
 describe('Text', () => {
@@ -9,10 +9,18 @@ describe('Text', () => {
     expect(tree).toMatchSnapshot();
   });
 
-  it('matches heading snapshot', () => {
+  it('matches h2 snapshot', () => {
     const tree = renderer.create(
       <H2>This is a heading</H2>
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+  it('matches h3 snapshot', () => {
+    const tree = renderer.create(
+      <H3>This is a heading</H3>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
 });
