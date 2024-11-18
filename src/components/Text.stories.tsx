afd026255a9c4d29c9cf18e3b4059909caf146b4
Josiah Ivey <josiah.ivey@gmail.com>
2024-11-18 07:09:06 -0800

    H3 (#60)
    * add an h3 heading
    
    * bump version
    
    * use updated color

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -1,9 +1,14 @@
-import { H2, Paragraph } from "./Text";
+import { H2, H3, Paragraph } from "./Text";
 
-export const PText = () => <>
-  <Paragraph>Body text</Paragraph>
-</>;
+export const PText = () => (
+  <>
+    <Paragraph>Body text</Paragraph>
+  </>
+);
 
-export const Heading = () => <>
-  <H2>Heading text</H2>
-</>;
+export const Heading = () => (
+  <>
+    <H2>H2 Heading text</H2>
+    <H3>H3 Heading text</H3>
+  </>
+);
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -7,6 +7,13 @@ export const H2 = styled.h2`
   font-weight: 700;
 `;
 
+export const H3 = styled.h3`
+  color: ${palette.neutralDarker};
+  font-size: 1.4rem;
+  font-weight: 700;
+  text-transform: uppercase;
+`;
+
 export const Paragraph = styled.p`
   color: ${palette.neutralDarker};
   font-size: 1.8rem;
