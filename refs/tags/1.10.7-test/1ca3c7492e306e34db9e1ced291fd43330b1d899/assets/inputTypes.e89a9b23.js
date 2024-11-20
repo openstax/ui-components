c3ee21e8ac686d33ce2575e2c83304d50ea6898b
staxly <staxly@users.noreply.github.com>
2024-11-20 22:47:29 +0000

    building refs/tags/1.10.7-test/1ca3c7492e306e34db9e1ced291fd43330b1d899

---
+++ refs/tags/1.10.7-test/1ca3c7492e306e34db9e1ced291fd43330b1d899/assets/inputTypes.e89a9b23.js
@@ -0,0 +1,36 @@
+import{s as i,a as t,F as h,j as s}from"./index.7c51418c.js";import"./Radio.829fba0c.js";import{C as y}from"./Checkbox.26f30184.js";const m=i.label`
+  display: flex;
+  flex-direction: column;
+  flex: 0;
+  justify-content: stretch;
+`,x=i.span`
+  white-space: nowrap;
+  font-weight: bold;
+  display: block;
+`,u=i.p`
+  font-style: italic;
+  margin: 0;
+  padding: 0;
+`,f=({value:n,...r})=>n?t(u,{...r,children:n}):null,w=n=>n.show?t(h,{children:"*"}):null,g=i.input`
+  flex: 1;
+  justify-content: stretch;
+`,b=i.div`
+  flex-direction: row;
+  display: flex;
+`,I=({label:n,addon:r,help:a,wrapperProps:o,onChangeValue:c,...e})=>s(m,{...o,children:[s(x,{children:[t(w,{show:e.required}),n,":"]}),s(b,{children:[t(g,{type:"text",...e,onChange:l=>{var d;c==null||c(e.transformValue?e.transformValue(l.target.value):l.target.value),(d=e.onChange)==null||d.call(e,l)}}),r]}),t(f,{value:a})]});i.div`
+  flex-direction: row;
+  display: flex;
+  align-items: center;
+`;i(x)`
+  white-space: normal;
+`;const v=i.div`
+  flex-direction: row;
+  display: flex;
+  align-items: center;
+`,j=i.p`
+  color: #C22032;
+  font-size: 1.4rem;
+  margin: 0;
+  padding: 0;
+  line-height: 2.5rem;
+`,q=({label:n,help:r,wrapperProps:a,error:o,onChangeValue:c,...e})=>s(m,{...a,children:[t(v,{children:t(y,{...e,onChange:l=>{var d;c==null||c(!!l.target.checked),(d=e.onChange)==null||d.call(e,l)},children:s(x,{children:[t(w,{show:e.required}),n]})})}),t(f,{value:r}),o!==void 0&&t(h,{children:o.map(l=>t(j,{children:l}))})]});export{q as C,I as T};
