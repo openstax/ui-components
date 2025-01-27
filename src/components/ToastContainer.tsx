082ab2b24db3e6b7d90ce5f992d5ea7bedb00733
Dante Soares <dante.m.soares@rice.edu>
2025-01-27 17:41:53 -0600

    DRY up the ToastContainer

--- src/components/ToastContainer.tsx
+++ src/components/ToastContainer.tsx
@@ -1,10 +1,13 @@
-import styled, { css } from 'styled-components';
+import styled, { css, ThemedStyledFunction } from 'styled-components';
 import { BodyPortal } from './BodyPortal';
 import { Toast } from './Toast';
 import { zIndex } from '../../src/theme';
 import { ToastData } from '../../src/types';
+import { ComponentType } from 'react';
 
-const StyledToastContainer = styled.div`
+const makeStyledToastContainer = <T extends keyof JSX.IntrinsicElements | ComponentType<any>>(
+  func: ThemedStyledFunction<T, object>
+) => func`
   ${(props: {inline: boolean}) => !props.inline && css`
     position: fixed;
     right: 2rem;
@@ -16,40 +19,38 @@ const StyledToastContainer = styled.div`
   gap: 1vh;
 `;
 
-const StyledBodyPortalToastContainer = styled(BodyPortal)`
-  ${(props: {inline: boolean}) => !props.inline && css`
-    position: fixed;
-    right: 2rem;
-  `}
-  z-index: ${zIndex.toasts};
-  display: grid;
-  justify-items: center;
-  justify-content: center;
-  gap: 1vh;
-`;
+const StyledToastContainer = makeStyledToastContainer(styled.div);
 
-export const ToastContainer = ({ toasts, onDismissToast, inline = false, className }: {
-  toasts: ToastData[], onDismissToast?: ToastData['onDismiss'], inline?: boolean, className?: string
-}) => {
-  return <StyledToastContainer inline={inline} aria-live="polite" slot='toast' className={className}>
-    {toasts.map((toast, index) => <Toast
+const StyledBodyPortalToastContainer = makeStyledToastContainer(styled(BodyPortal));
+
+export type ToastContainerParams = {
+  toasts: ToastData[];
+  onDismissToast?: ToastData['onDismiss'];
+  inline?: boolean;
+  className?: string;
+};
+
+export type ToastContainerComponent = (params: ToastContainerParams) => JSX.Element;
+
+const makeToasts = (toasts: ToastData[], inline: boolean, onDismissToast?: (id: string | undefined) => void) => (
+  toasts.map((toast, index) =>
+    <Toast
       key={`toast-${index}`}
       onDismiss={onDismissToast}
       inline={inline}
       {...toast}
-      >{toast.message}</Toast>)}
+      >{toast.message}</Toast>
+  )
+);
+
+export const ToastContainer: ToastContainerComponent = ({ toasts, onDismissToast, inline = false, className }) => (
+  <StyledToastContainer inline={inline} aria-live='polite' className={className}>
+    {makeToasts(toasts, inline, onDismissToast)}
   </StyledToastContainer>
-};
+);
 
-export const BodyPortalToastContainer = ({ toasts, onDismissToast, inline = false, className }: {
-  toasts: ToastData[], onDismissToast?: ToastData['onDismiss'], inline?: boolean, className?: string
-}) => {
-  return <StyledBodyPortalToastContainer inline={inline} aria-live="polite" slot='toast' className={className}>
-    {toasts.map((toast, index) => <Toast
-      key={`toast-${index}`}
-      onDismiss={onDismissToast}
-      inline={inline}
-      {...toast}
-      >{toast.message}</Toast>)}
+export const BodyPortalToastContainer: ToastContainerComponent = ({ toasts, onDismissToast, inline = false, className }) => (
+  <StyledBodyPortalToastContainer inline={inline} aria-live='polite' slot='toast' className={className}>
+    {makeToasts(toasts, inline, onDismissToast)}
   </StyledBodyPortalToastContainer>
-};
+);
