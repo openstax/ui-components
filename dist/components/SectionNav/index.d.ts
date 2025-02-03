27fef0ba8fbebe0b03020196ebc830f1c3afb1f2
jomcarvajal <jose.carvajal@edify.cr>
2025-02-03 14:43:06 -0600

    dist

---
+++ dist/components/SectionNav/index.d.ts
@@ -0,0 +1,9 @@
+import React from "react";
+export interface SectionNavProps {
+    children: React.ReactNode;
+    handlePrevArrow: () => void;
+    handleNextArrow: () => void;
+    isPrevArrowDisabled?: boolean;
+    isNextArrowDisabled?: boolean;
+}
+export declare const SectionNav: ({ children, handlePrevArrow, handleNextArrow, isPrevArrowDisabled, isNextArrowDisabled }: SectionNavProps) => import("react/jsx-runtime").JSX.Element;
