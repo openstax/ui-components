fbb04c8cd195681defffa79f75108bba4e76e3a8
jomcarvajal <jose.carvajal@edify.cr>
2025-02-04 16:13:06 -0600

    dist

---
+++ dist/theme/buttons.d.ts
@@ -0,0 +1,15 @@
+export declare type ButtonVariant = keyof typeof buttonStyleSets;
+interface ButtonStyleSet {
+    background: string;
+    backgroundActive: string;
+    backgroundHover: string;
+    color: string;
+    fontWeight?: number;
+}
+declare const buttonStyleSets: {
+    readonly primary: ButtonStyleSet;
+    readonly light: ButtonStyleSet;
+    readonly secondary: ButtonStyleSet;
+};
+export declare const applyButtonVariantStyles: (variant: ButtonVariant) => import("styled-components").FlattenSimpleInterpolation;
+export {};
