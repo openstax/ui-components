6defb845e4e3158060aeeb79892b2aa6775f6bb7
tom <tom.g.woodward@gmail.com>
2025-05-01 10:59:02 -0400

    add pagination element

---
+++ src/components/Pagination.stories.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Pagination, LinkForPage } from "./Pagination";
+
+export const Examples = () => {
+  const [currentPage, setCurrentPage] = React.useState(1);
+  return <div>
+    <h2>Default settings</h2>
+    <Pagination
+      currentPage={currentPage} totalPages={10}
+      Page={({ page, current }) =>
+        <LinkForPage page={page} current={current} onClick={() => setCurrentPage(page)} />
+      }
+    />
+
+    <h2>Showing only one link from the end</h2>
+    <Pagination
+      currentPage={currentPage} totalPages={10} showFromEnd={1}
+      Page={({ page, current }) =>
+        <LinkForPage page={page} current={current} onClick={() => setCurrentPage(page)} />
+      }
+    />
+
+    <h2>Showing zero links from the end</h2>
+    <Pagination
+      currentPage={currentPage} totalPages={10} showFromEnd={0}
+      Page={({ page, current }) =>
+        <LinkForPage page={page} current={current} onClick={() => setCurrentPage(page)} />
+      }
+    />
+
+    <h2>Showing only one link from the end and current</h2>
+    <Pagination
+      currentPage={currentPage} totalPages={10} showFromEnd={1} showFromCurrent={1}
+      Page={({ page, current }) =>
+        <LinkForPage page={page} current={current} onClick={() => setCurrentPage(page)} />
+      }
+    />
+    
+    <h2>less links</h2>
+    <Pagination
+      currentPage={currentPage} totalPages={2}
+      Page={({ page, current }) =>
+        <LinkForPage page={page} current={current} onClick={() => setCurrentPage(page)} />
+      }
+    />
+    
+    <h2>more links and summary</h2>
+    <Pagination
+      currentPage={currentPage} totalPages={40} totalItems={395} pageSize={10}
+      Page={({ page, current }) =>
+        <LinkForPage page={page} current={current} onClick={() => setCurrentPage(page)} />
+      }
+    />
+    
+    <h2>zero links</h2>
+    <Pagination
+      currentPage={currentPage} totalPages={0}
+      Page={({ page, current }) =>
+        <LinkForPage page={page} current={current} onClick={() => setCurrentPage(page)} />
+      }
+    />
+    
+    <h2>one link</h2>
+    <Pagination
+      currentPage={currentPage} totalPages={1}
+      Page={({ page, current }) =>
+        <LinkForPage page={page} current={current} onClick={() => setCurrentPage(page)} />
+      }
+    />
+  </div>
+};
