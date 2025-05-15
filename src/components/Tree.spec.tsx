be1777d7d09625347e63c5b5a800a99e955639a9
Josiah Ivey <josiah.ivey@gmail.com>
2025-05-15 13:28:19 -0700

    Tree component (#87)
    * use nightly version with fixed types
    
    * basic tree styles
    
    * export
    
    * tree snapshot
    
    * snapshot updates from rac build

---
+++ src/components/Tree.spec.tsx
@@ -0,0 +1,48 @@
+import { render } from "@testing-library/react";
+import { Tree, TreeChevron, TreeItem, TreeItemContent } from './Tree';
+import { Checkbox } from './Checkbox';
+
+describe('Tree', () => {
+  it('matches snapshot', () => {
+    const { asFragment } = render(
+      <Tree>
+        <TreeItem id="1" textValue="1">
+          <TreeItemContent>
+            <Checkbox
+              value={"one"}
+              slot="check"
+              size={1.4}
+            >
+               First
+            </Checkbox>
+            <TreeChevron>Show/Hide</TreeChevron>
+          </TreeItemContent>
+          <TreeItem id="2" textValue="2">
+            <TreeItemContent>
+              <Checkbox
+                value={"two"}
+                slot="check"
+                size={1.4}
+              >
+                 Second
+              </Checkbox>
+              <TreeChevron>Show/Hide</TreeChevron>
+            </TreeItemContent>
+            <TreeItem id="3" textValue="3">
+              <TreeItemContent>
+                <Checkbox
+                  value={"third"}
+                  slot="check"
+                  size={1.4}
+                >
+                   Third
+                </Checkbox>
+              </TreeItemContent>
+            </TreeItem>
+          </TreeItem>
+        </TreeItem>
+      </Tree>
+    )
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
---
+++ src/components/Tree.stories.tsx
@@ -0,0 +1,42 @@
+import { Checkbox } from "./Checkbox";
+import { Tree, TreeChevron, TreeItem, TreeItemContent } from "./Tree";
+
+export const Default = () => {
+  return <Tree>
+  <TreeItem id="1" textValue="1">
+    <TreeItemContent>
+      <Checkbox
+        value={"one"}
+        slot="check"
+        size={1.4}
+      >
+        First
+      </Checkbox>
+      <TreeChevron>Show/Hide</TreeChevron>
+    </TreeItemContent>
+    <TreeItem id="2" textValue="2">
+      <TreeItemContent>
+        <Checkbox
+          value={"two"}
+          slot="check"
+          size={1.4}
+        >
+           Second
+        </Checkbox>
+        <TreeChevron>Show/Hide</TreeChevron>
+      </TreeItemContent>
+      <TreeItem id="3" textValue="3">
+        <TreeItemContent>
+          <Checkbox
+            value={"third"}
+            slot="check"
+            size={1.4}
+          >
+             Third
+          </Checkbox>
+        </TreeItemContent>
+      </TreeItem>
+    </TreeItem>
+  </TreeItem>
+</Tree>
+};
