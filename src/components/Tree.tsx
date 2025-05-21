a023e3bec2730f4423613e474ee86a7e2fc845b8
jomcarvajal <jose.carvajal@edify.cr>
2025-05-21 14:54:06 -0600

    add click handler

--- src/components/Tree.tsx
+++ src/components/Tree.tsx
@@ -1,6 +1,7 @@
 import * as RAC from "react-aria-components";
 import { colors } from "../theme";
 import styled from "styled-components";
+import { MouseEventHandler } from "react";
 
 export const Tree = styled(RAC.Tree)`
   padding: 0.8rem;
@@ -22,32 +23,45 @@ export const TreeItem = styled(RAC.TreeItem)`
 export const TreeItemContent = styled(RAC.TreeItemContent)`
 `;
 
-const ChevronButton = styled.button`
-  all: unset;
+const ChevronWrapper = styled.div`
   display: flex;
+  flex-direction: row;
   align-items: center;
-  justify-content: center;
-  width: 1.4rem;
-  height: 100%;
+  padding: 1rem 0 1rem 3rem;
+  font-weight: bold;
+  gap: 0.8rem;
 
-  svg {
-    rotate: 0deg;
-    transition: rotate 200ms;
+  button {
+    all: unset;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     width: 1.4rem;
-    height: 1.4rem;
-    stroke: ${colors.palette.neutralDarker};
-    stroke-width: 0.3rem;
+    height: 100%;
+
+    svg {
+      rotate: 0deg;
+      transition: rotate 200ms;
+      width: 1.4rem;
+      height: 1.4rem;
+      stroke: ${colors.palette.neutralDarker};
+      stroke-width: 0.3rem;
+    }
   }
 `;
 
-export const TreeChevron = styled((props: React.PropsWithChildren<{ className?: string }>) => (
-  <>
-    <ChevronButton className={props.className} type="button" aria-label="expand/collapse">
-      <svg viewBox="0 0 24 24">
-        <path d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-      </svg>
-    </ChevronButton>
-    {props.children}
-  </>
+export const TreeChevron = styled(
+  (props: React.PropsWithChildren<{ 
+    className?: string,
+    onClickHandler?: MouseEventHandler<HTMLButtonElement> | undefined,
+  }>) => (
+  <ChevronWrapper>
+  <button className={props.className} type="button" aria-label="expand/collapse" onClick={props.onClickHandler}>
+    <svg viewBox="0 0 24 24">
+      <path d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+    </svg>
+  </button>
+  {props.children}
+  </ChevronWrapper>
 ))`
 `;
