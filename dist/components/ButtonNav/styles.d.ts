37f4f008ab74ec4bd260fd8ca1ca8c1fc00d9a22
jomcarvajal <jose.carvajal@edify.cr>
2025-05-30 15:45:51 -0600

    dist

---
+++ dist/components/ButtonNav/styles.d.ts
@@ -0,0 +1,6 @@
+export declare const ButtonNavContainer: import("styled-components").StyledComponent<"div", any, {}, never>;
+export declare const ButtonNavWrapper: import("styled-components").StyledComponent<"div", any, {}, never>;
+export declare const ButtonNavGroup: import("styled-components").StyledComponent<"div", any, {}, never>;
+export declare const StyledArrow: import("styled-components").StyledComponent<"button", any, {
+    disabled: boolean;
+}, never>;
