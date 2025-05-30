017bb7f93ac9c0bf09a9370cdc331391191c3320
jomcarvajal <jose.carvajal@edify.cr>
2025-05-30 14:03:34 -0600

    dist

---
+++ dist/components/Button.stories.d.ts
@@ -0,0 +1,5 @@
+export declare const Primary: () => import("react/jsx-runtime").JSX.Element;
+export declare const Light: () => import("react/jsx-runtime").JSX.Element;
+export declare const Secondary: () => import("react/jsx-runtime").JSX.Element;
+export declare const Link: () => import("react/jsx-runtime").JSX.Element;
+export declare const Linkish: () => import("react/jsx-runtime").JSX.Element;
