bb0b335131d39346ef5b732ea3ad209701a5c316
jomcarvajal <jose.carvajal@edify.cr>
2025-06-01 23:30:33 -0600

    dist

---
+++ dist/components/Checkbox/CheckboxRAC.stories.d.ts
@@ -0,0 +1,4 @@
+export declare const Primary: () => import("react/jsx-runtime").JSX.Element;
+export declare const Light: () => import("react/jsx-runtime").JSX.Element;
+export declare const Error: () => import("react/jsx-runtime").JSX.Element;
+export declare const Disabled: () => import("react/jsx-runtime").JSX.Element;
