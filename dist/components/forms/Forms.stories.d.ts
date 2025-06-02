a0fb277affe5e9d4d0d939ae78e17fad9b14848a
jomcarvajal <jose.carvajal@edify.cr>
2025-06-02 00:25:55 -0600

    dist

---
+++ dist/components/forms/Forms.stories.d.ts
@@ -0,0 +1,4 @@
+export declare const BasicControlledForm: () => import("react/jsx-runtime").JSX.Element;
+export declare const FormRepeatableElements: () => import("react/jsx-runtime").JSX.Element;
+export declare const BasicDataReferences: () => import("react/jsx-runtime").JSX.Element;
+export declare const DataReferencesInNamespaces: () => import("react/jsx-runtime").JSX.Element;
