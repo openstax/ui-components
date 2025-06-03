0062989b54f33410e071f4d526e66ca054c01da9
jomcarvajal <jose.carvajal@edify.cr>
2025-06-03 11:33:31 -0600

    update tree story

--- src/components/Tree/Tree.stories.tsx
+++ src/components/Tree/Tree.stories.tsx
@@ -1,42 +1,55 @@
+import React from "react";
 import { TreeCheckbox as Checkbox } from "./TreeCheckbox";
 import { Tree, TreeChevron, TreeItem, TreeItemContent } from "./Tree";
 
+
+
+import type { Selection } from "@react-types/shared";
+
 export const Default = () => {
-  return <Tree>
-  <TreeItem id="1" textValue="1">
-    <TreeItemContent>
-      <Checkbox
-        value={"1"}
-        slot="selection"
-        size={1.4}
-      >
-        First
-      </Checkbox>
-      <TreeChevron>Show/Hide</TreeChevron>
-    </TreeItemContent>
-    <TreeItem id="2" textValue="2">
-      <TreeItemContent>
-        <Checkbox
-          value={"2"}
-          slot="selection"
-          size={1.4}
-        >
-           Second
-        </Checkbox>
-        <TreeChevron>Show/Hide</TreeChevron>
-      </TreeItemContent>
-      <TreeItem id="3" textValue="3">
+  const [selectedKeys, setSelectedKeys] = React.useState<Selection>(new Set());
+
+  return (
+    <Tree
+      selectedKeys={selectedKeys}
+      onSelectionChange={setSelectedKeys}
+      selectionMode="multiple"
+    >
+      <TreeItem id="one" textValue="First">
         <TreeItemContent>
           <Checkbox
-            value={"3"}
+            value={"one"}
             slot="selection"
             size={1.4}
           >
-             Third
+            First
           </Checkbox>
+          <TreeChevron>Show/Hide</TreeChevron>
         </TreeItemContent>
+        <TreeItem id="two" textValue="Second">
+          <TreeItemContent>
+            <Checkbox
+              value={"two"}
+              slot="selection"
+              size={1.4}
+            >
+              Second
+            </Checkbox>
+            <TreeChevron>Show/Hide</TreeChevron>
+          </TreeItemContent>
+          <TreeItem id="three" textValue="Third">
+            <TreeItemContent>
+              <Checkbox
+                value={"three"}
+                slot="selection"
+                size={1.4}
+              >
+                Third
+              </Checkbox>
+            </TreeItemContent>
+          </TreeItem>
+        </TreeItem>
       </TreeItem>
-    </TreeItem>
-  </TreeItem>
-</Tree>
+    </Tree>
+  );
 };
