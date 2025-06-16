e2e400452681748abcf1fe7dedb1615142828af1
jomcarvajal <jose.carvajal@edify.cr>
2025-06-16 09:16:24 -0600

    dist

---
+++ dist/components/ErrorBoundary.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="react" />
+import * as Sentry from '@sentry/react';
+import type { ErrorBoundaryProps } from '@sentry/react/types/errorboundary';
+export declare const ErrorBoundary: ({ children, renderFallback, fallback, sentryDsn, sentryInit, ...props }: ErrorBoundaryProps & {
+    renderFallback?: boolean | undefined;
+    sentryDsn?: string | undefined;
+    sentryInit?: Sentry.BrowserOptions | undefined;
+    errorFallbacks?: {
+        [_: string]: JSX.Element;
+    } | undefined;
+    errorLevels?: {
+        [_: string]: Sentry.SeverityLevel;
+    } | undefined;
+}) => import("react/jsx-runtime").JSX.Element;
