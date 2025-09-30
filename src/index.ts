6e6ec494eed61d42e0b823c29a3df90a6edcd777
jomcarvajal <jose.carvajal@edify.cr>
2025-09-30 14:41:00 -0600

    export MessageBox styles

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ export * from './components/Tree/TreeCheckbox';
 export * from './components/DropdownMenu';
 export * from './components/Error';
 export * from './components/MessageBox';
+export * from './components/MessageBox.styles';
 export * from './components/ErrorBoundary';
 export * from './components/ErrorMessage';
 export * from './components/ErrorModal';
