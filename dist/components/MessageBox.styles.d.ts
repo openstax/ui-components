dcda4046513ad72342303e4f12036dd096443848
jomcarvajal <jose.carvajal@edify.cr>
2025-09-30 16:05:41 -0600

    dist

---
+++ dist/components/MessageBox.styles.d.ts
@@ -0,0 +1,6 @@
+export declare const BoxWrapper: import("styled-components").StyledComponent<"div", any, {
+    margin?: string | undefined;
+}, never>;
+export declare const BoxHeading: import("styled-components").StyledComponent<"h3", any, {}, never>;
+export declare const BoxBody: import("styled-components").StyledComponent<"div", any, {}, never>;
+export declare const BoxEventId: import("styled-components").StyledComponent<"div", any, {}, never>;
