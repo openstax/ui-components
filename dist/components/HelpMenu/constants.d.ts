8d9cabd6840905cf03d59faaf194fb07400a0130
Tyler Nullmeier <tylerzeromaster@gmail.com>
2025-10-06 14:45:58 -0500

    dist

---
+++ dist/components/HelpMenu/constants.d.ts
@@ -0,0 +1,20 @@
+export declare const orgId = "00DU0000000Kwch";
+export declare const app = "Web_Messaging_Deployment";
+export declare const deployment = "ESWWebMessagingDeployme1716235390398";
+export declare const deploymentURL: string;
+export declare const scrt2URL = "https://openstax.my.salesforce-scrt.com";
+export declare const scriptUrl: string;
+export declare const businessHoursURL: string;
+export declare const chatEmbedDefaults: {
+    readonly orgId: "00DU0000000Kwch";
+    readonly app: "Web_Messaging_Deployment";
+    readonly deploymentURL: string;
+    readonly scrt2URL: "https://openstax.my.salesforce-scrt.com";
+    readonly scriptUrl: string;
+    readonly businessHoursURL: string;
+};
+export declare const embeddedChatEvents: {
+    readonly READY: "onEmbeddedMessagingReady";
+    readonly BUSINESS_HOURS_STARTED: "onEmbeddedMessagingBusinessHoursStarted";
+    readonly BUSINESS_HOURS_ENDED: "onEmbeddedMessagingBusinessHoursEnded";
+};
