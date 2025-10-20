80cba9762f71d88190d86a59e206f3d93f2fe6b2
TylerZeroMaster <33585550+TylerZeroMaster@users.noreply.github.com>
2025-10-20 11:08:01 -0500

    Core 954 window help menu (#103)
    * Add pre-chat fields
    
    * Update snapshot for embed service event listeners
    
    * Set hidden pre-caht fields immediately if ready
    
    These might change after rendering, in which case they should be set immediately if we were ready once before.
    
    * More complete handling of prechat fields
    
    * Add 10 second grace period for business hours
    
    * Fetch business hours on become visible too
    
    * Make grace period adjustable
    
    * Pre-fill school field if possible; minor refactor
    
    * Avoid extra renders
    
    * Use external business hours and embed
    
    * Use less nesting in chat configuration
    
    * Remove unused constants
    
    * Timezones in tests ...
    
    * Cleanup some lingering comments
    
    * Typo - strict comparison
    
    * Minor version

---
+++ src/components/HelpMenu/index.spec.tsx
@@ -0,0 +1,97 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { BodyPortalSlotsContext } from '../BodyPortalSlotsContext';
+import { HelpMenu, HelpMenuItem, HelpMenuProps } from '.';
+import { NavBar } from '../NavBar';
+import { ChatConfiguration } from './hooks';
+
+describe('HelpMenu', () => {
+  let root: HTMLElement;
+
+  beforeAll(() => {
+    global.CSS = {
+      supports: () => true,
+      escape: jest.fn(),
+    } as any;
+    jest.useFakeTimers();
+    jest.setSystemTime(0);
+  });
+
+  beforeEach(() => {
+    root = document.createElement('main');
+    root.id = 'root';
+    document.body.append(root);
+  });
+
+  it('matches snapshot', async () => {
+    render(
+      <BodyPortalSlotsContext.Provider value={['nav', 'root']}>
+        <NavBar logo>
+          <HelpMenu contactFormParams={[{key: 'userId', value: 'test'}]}>
+            <HelpMenuItem onAction={() => window.alert('Ran HelpMenu callback function')}>
+              Test Callback
+            </HelpMenuItem>
+          </HelpMenu>
+        </NavBar>
+      </BodyPortalSlotsContext.Provider>
+    );
+
+    // Reveal the default button in the help menu
+    fireEvent.click(await screen.findByText('Help'));
+    screen.getByText(/Report an issue/i);
+
+    expect(document.body).toMatchSnapshot();
+  });
+
+  it('errors if the service is unavailable', async () => {
+    const consoleSpy = jest.spyOn(console, 'error').mockImplementation(() => {
+      // SILENCE
+    });
+    const errorResponse: ChatConfiguration['err'] = {
+      type: 'test',
+      detail: 'test'
+    };
+    const chatEmbedPath = 'https://example.com/';
+    const chatEmbedParams: HelpMenuProps['chatConfig'] = {chatEmbedPath, err: errorResponse};
+
+    render(
+      <BodyPortalSlotsContext.Provider value={['nav', 'root']}>
+        <NavBar logo>
+          <HelpMenu chatConfig={chatEmbedParams} contactFormParams={[{key: 'userId', value: 'test'}, {key: 'other', value: 'param'}]}>
+            <HelpMenuItem onAction={() => window.alert('Ran HelpMenu callback function')}>
+              Test Callback
+            </HelpMenuItem>
+          </HelpMenu>
+        </NavBar>
+      </BodyPortalSlotsContext.Provider>
+    );
+    fireEvent.click(await screen.findByText('Help'));
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('replaces button within hours', async () => {
+    const happyHoursResponse: ChatConfiguration['businessHours'] = {
+      businessHoursInfo: {
+        businessHours: [
+          { startTime: Date.now() - 60_000, endTime: Date.now() + 1_440_000 }
+        ]
+      },
+      timestamp: Date.now(),
+    };
+    const chatEmbedPath = 'https://example.com/';
+    const chatEmbedParams: HelpMenuProps['chatConfig'] = {chatEmbedPath, businessHours: happyHoursResponse};
+  
+    render(
+      <BodyPortalSlotsContext.Provider value={['nav', 'root']}>
+        <NavBar logo>
+          <HelpMenu chatConfig={chatEmbedParams} contactFormParams={[{key: 'userId', value: 'test'}, {key: 'other', value: 'param'}]}>
+            <HelpMenuItem onAction={() => window.alert('Ran HelpMenu callback function')}>
+              Test Callback
+            </HelpMenuItem>
+          </HelpMenu>
+        </NavBar>
+      </BodyPortalSlotsContext.Provider>
+    );
+    fireEvent.click(await screen.findByText('Help'));
+    await screen.findByRole('menuitem', { name: /chat with us/i });
+  });
+});
