ad811b533659b801e80ce6bc1796d2fa68220155
tom <tom.g.woodward@gmail.com>
2026-02-19 16:50:52 -0500

    remove build script override

--- scripts/build.bash
+++ scripts/build.bash
@@ -7,9 +7,3 @@ project_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd 
 cd "$project_dir"
 
 microbundle
-  
-if [[ "$(uname)" == "Darwin" ]]; then
-  sed -i '' 's/jsx-runtime/jsx-runtime.js/g' ./dist/index.modern.mjs
-else
-  sed -i 's/jsx-runtime/jsx-runtime.js/g' ./dist/index.modern.mjs
-fi
