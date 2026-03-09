133ec7dec9d1e733cdc884fe96845be9c025d83e
Roy Johnson <roy.e.johnson@rice.edu>
2026-03-09 11:00:23 -0500

    dist

---
+++ dist/components/Loader.stories.d.ts
@@ -0,0 +1,3 @@
+export declare const Default: () => import("react/jsx-runtime").JSX.Element;
+export declare const Large: () => import("react/jsx-runtime").JSX.Element;
+export declare const Delay: () => import("react/jsx-runtime").JSX.Element;
---
+++ dist/components/Tabs.d.ts
@@ -0,0 +1,24 @@
+import React from "react";
+import * as RAC from "react-aria-components";
+export declare type TabsProps = {
+    variant?: "button-bar";
+    size?: "large" | "medium" | "small";
+    className?: string;
+    children?: React.ReactNode;
+} & RAC.TabsProps;
+export declare const tabListBaseCss = "\n  overflow-x: auto;\n  overscroll-behavior: contain;\n  display: flex;\n  flex-direction: row;\n";
+export declare const tabBaseCss: import("styled-components").FlattenInterpolation<import("styled-components").ThemedStyledProps<{
+    variant?: "button-bar" | undefined;
+    size?: "small" | "large" | "medium" | undefined;
+    className?: string | undefined;
+    children?: React.ReactNode;
+} & RAC.TabsProps, any>>;
+export declare const buttonBarWrapperCss: string;
+export declare const buttonBarItemCss: import("styled-components").FlattenInterpolation<import("styled-components").ThemedStyledProps<{
+    variant?: "button-bar" | undefined;
+    size?: "small" | "large" | "medium" | undefined;
+    className?: string | undefined;
+    children?: React.ReactNode;
+} & RAC.TabsProps, any>>;
+export declare const Tabs: ({ variant, size, className, children, ...restProps }: TabsProps) => import("react/jsx-runtime").JSX.Element;
+export { TabList, Tab, TabPanel } from "react-aria-components";
