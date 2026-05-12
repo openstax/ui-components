73318dab5a99f67f82d59893cf03a7ac1c940bf9
tom <tom.g.woodward@gmail.com>
2026-05-12 13:12:19 -0400

    replace microbundle with tsc only build

--- scripts/build.bash
+++ scripts/build.bash
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
-# spell-checker: ignore pipefail
+# spell-checker: ignore pipefail mkdir
 set -euo pipefail; if [ -n "${DEBUG-}" ]; then set -x; fi
 
 project_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 
 cd "$project_dir"
 
-microbundle
+tsc_args=(--noEmit false --declaration)
+
+for arg in "$@"; do
+  case "$arg" in
+    --watch) tsc_args+=(--watch --preserveWatchOutput) ;;
+  esac
+done
+
+mkdir -p dist
+
+yarn -s tsc --project tsconfig.without-specs.esm.json "${tsc_args[@]}"
+yarn -s tsc --project tsconfig.without-specs.cjs.json "${tsc_args[@]}"
