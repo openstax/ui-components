2c8c8fca172d29ac5081b7a7f8a643ebcea31ffd
tom <tom.g.woodward@gmail.com>
2026-05-13 14:08:49 -0400

    simplify build script

--- scripts/build.bash
+++ scripts/build.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# spell-checker: ignore pipefail mkdir
+# spell-checker: ignore pipefail
 set -euo pipefail; if [ -n "${DEBUG-}" ]; then set -x; fi
 
 project_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
@@ -7,14 +7,6 @@ project_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd 
 cd "$project_dir"
 
 tsc_args=(--noEmit false --declaration)
-
-for arg in "$@"; do
-  case "$arg" in
-    --watch) tsc_args+=(--watch --preserveWatchOutput) ;;
-  esac
-done
-
-mkdir -p dist
 
 yarn -s tsc --project tsconfig.without-specs.esm.json "${tsc_args[@]}"
 yarn -s tsc --project tsconfig.without-specs.cjs.json "${tsc_args[@]}"
