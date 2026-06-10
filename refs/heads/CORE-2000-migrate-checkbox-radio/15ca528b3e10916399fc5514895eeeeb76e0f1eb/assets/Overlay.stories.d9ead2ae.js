2dd29113b9f214df584a394ec5d6e440f325e524
staxly <staxly@users.noreply.github.com>
2026-06-10 23:11:07 +0000

    building refs/heads/CORE-2000-migrate-checkbox-radio/15ca528b3e10916399fc5514895eeeeb76e0f1eb

---
+++ refs/heads/CORE-2000-migrate-checkbox-radio/15ca528b3e10916399fc5514895eeeeb76e0f1eb/assets/Overlay.stories.d9ead2ae.js
@@ -0,0 +1,21 @@
+import{s as o,a as e,j as l,r as d}from"./index.c591c584.js";import{c,C as m,d as p}from"./Modal.a7af1ed3.js";import{k as u}from"./Dialog.module.4707fa55.js";import"./theme.469067e1.js";import"./palette.5e342580.js";import"./VisuallyHidden.module.fe09bc7a.js";import"./useFocusRing.module.1ba3e8e4.js";import"./OverlayArrow.module.8839bbf8.js";import"./context.module.973db1e1.js";import"./Button.module.29e92863.js";import"./useButton.module.dd4fe412.js";import"./Collection.module.d6889986.js";import"./useTreeState.module.7d36eff0.js";const f=o(c)`
+  background-color: rgba(0, 0, 0, 0.89);
+`,h=o(m)`
+  height: 4rem;
+  width: 4rem;
+  position: absolute;
+  right: 2em;
+  top: 2em;
+`,y=o(p)`
+  color: #fff;
+`,w=o(u)`
+  position: relative;
+  flex-grow: 1;
+  height: 100%;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  outline: none;
+`,v=({className:r,onClose:t,children:n,show:i,...s})=>i?e(f,{className:r,isDismissable:!0,onOpenChange:a=>!a&&t(),...s,children:l(y,{defaultOpen:!0,children:[e(h,{onClick:t,variant:"inverted-circle"}),e(w,{children:n})]})}):null,W=()=>{const[r,t]=d.exports.useState(!0);return e(v,{onClose:()=>t(!1),show:r,children:e("h1",{children:"Overlay"})})};typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{W as Default};
