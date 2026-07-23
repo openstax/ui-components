b155b090e8c021a4e7bcbce14919e45f48d9c82d
staxly <staxly@users.noreply.github.com>
2026-07-23 18:00:31 +0000

    building refs/heads/CORE-2007-migrate-manage-cookies/7f9703ce14fc5f65830a40057b5d24744bafd6fa

---
+++ refs/heads/CORE-2007-migrate-manage-cookies/7f9703ce14fc5f65830a40057b5d24744bafd6fa/assets/Radio.stories.aac5efd7.js
@@ -0,0 +1,8 @@
+import{s as n,a as e,F as d,j as o}from"./index.c552fa20.js";import{R as i}from"./Radio.15eebb58.js";import"./theme.469067e1.js";import"./palette.5e342580.js";import"./Tooltip.4605b903.js";import"./Button.module.f85357f8.js";import"./useFocusRing.module.ca71fda0.js";import"./context.module.f2356bc4.js";import"./useButton.module.8b9c3e90.js";import"./OverlayArrow.module.14877997.js";import"./Info.339c3af4.js";const a=n.div`
+  & + & {
+    margin-top: 3.2rem;
+  }
+  > * + * {
+    margin-top: 0.5rem;
+  }
+`,r=t=>o(a,{children:[e(i,{...t,children:"Label"}),e(i,{...t,defaultChecked:!0,children:"Label"}),e(i,{...t,children:"Label"}),e(i,{disabled:!0,...t,children:"Disabled label"})]}),D=()=>e(d,{children:r({name:"default"})}),R=()=>e(d,{children:r({name:"withTooltip",tooltipText:"Tooltip text for radio input goes here"})}),l=t=>o(a,{children:[e(i,{disabled:!0,...t,children:"Disabled label"}),e(i,{disabled:!0,defaultChecked:!0,...t,children:"Disabled label"})]}),g=()=>o(d,{children:[l({name:"disabled"}),l({name:"disabledWithTooltip",tooltipText:"Tooltip text for disabled radio"})]});typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{D as Default,g as Disabled,R as WithTooltip};
