024e985ff63486fedcc03ae65d79ae0e5b366504
Josiah Ivey <josiah.ivey@gmail.com>
2026-09-21 12:37:37 -0700

    restore formatting

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -19,41 +19,23 @@ export const Primary = () => {
   );
 };
 
-export const Light = () => (
-  <>
-    <Button variant="light">Light</Button>
-    <Button variant="light" disabled>
-      Disabled
-    </Button>
-    <Button variant="light" isWaiting={true} waitingText="Waiting...">
-      Button
-    </Button>
-  </>
-);
+export const Light = () => <>
+  <Button variant='light'>Light</Button>
+  <Button variant='light' disabled>Disabled</Button>
+  <Button variant='light' isWaiting={true} waitingText='Waiting...'>Button</Button>
+</>;
 
-export const Secondary = () => (
-  <>
-    <Button variant="secondary">Secondary</Button>
-    <Button variant="secondary" disabled>
-      Disabled
-    </Button>
-    <Button variant="secondary" isWaiting={true} waitingText="Waiting...">
-      Button
-    </Button>
-  </>
-);
+export const Secondary = () => <>
+  <Button variant='secondary'>Secondary</Button>
+  <Button variant='secondary' disabled>Disabled</Button>
+  <Button variant='secondary' isWaiting={true} waitingText='Waiting...'>Button</Button>
+</>;
 
-export const Link = () => (
-  <>
-    <LinkButton href="https://openstax.org">Primary</LinkButton>
-    <LinkButton variant="secondary" href="https://openstax.org" target="_blank">
-      Button
-    </LinkButton>
-  </>
-);
+export const Link = () => <>
+  <LinkButton href='https://openstax.org'>Primary</LinkButton>
+  <LinkButton variant='secondary' href='https://openstax.org' target='_blank'>Button</LinkButton>
+</>;
 
-export const Linkish = () => (
-  <>
-    <ButtonLink onClick={() => alert('hi')}>press to say hi</ButtonLink>
-  </>
-);
+export const Linkish = () => <>
+  <ButtonLink onClick={() => alert('hi')}>press to say hi</ButtonLink>
+</>;
