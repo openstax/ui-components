1d08097971ab96925c6db1c534d502b9e0570d57
Josiah Ivey <josiah.ivey@gmail.com>
2026-09-21 12:17:43 -0700

    fix button not wrapping text
    This fixes an issue with the button text not wrapping when the button
    becomes longer than the width of the viewport, creating overflow. It
    should instead wrap to a new line (and not be limited by height.) I
    also customized the list of ladle viewport sizes to include 320, it
    will hopefully make it easier to test that minimum size to spot issues
    like this in the future.

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -1,28 +1,59 @@
-import { Button, LinkButton, ButtonLink } from "./Button";
+import React from 'react';
+import { Button, LinkButton, ButtonLink } from './Button';
+import { Input } from 'react-aria-components';
 
-export const Primary = () => <>
-  <Button>Primary</Button>
-  <Button disabled>Disabled</Button>
-  <Button isWaiting={true} waitingText='Waiting...'>Button</Button>
-</>;
+export const Primary = () => {
+  const [state, setState] = React.useState('Primary');
 
-export const Light = () => <>
-  <Button variant='light'>Light</Button>
-  <Button variant='light' disabled>Disabled</Button>
-  <Button variant='light' isWaiting={true} waitingText='Waiting...'>Button</Button>
-</>;
+  return (
+    <>
+      <Input value={state} onChange={e => setState(e.target.value)} />
+      <br />
+      <br />
+      <Button>{state}</Button>
+      <Button disabled>Disabled</Button>
+      <Button isWaiting={true} waitingText="Waiting...">
+        Button
+      </Button>
+    </>
+  );
+};
 
-export const Secondary = () => <>
-  <Button variant='secondary'>Secondary</Button>
-  <Button variant='secondary' disabled>Disabled</Button>
-  <Button variant='secondary' isWaiting={true} waitingText='Waiting...'>Button</Button>
-</>;
+export const Light = () => (
+  <>
+    <Button variant="light">Light</Button>
+    <Button variant="light" disabled>
+      Disabled
+    </Button>
+    <Button variant="light" isWaiting={true} waitingText="Waiting...">
+      Button
+    </Button>
+  </>
+);
 
-export const Link = () => <>
-  <LinkButton href='https://openstax.org'>Primary</LinkButton>
-  <LinkButton variant='secondary' href='https://openstax.org' target='_blank'>Button</LinkButton>
-</>;
+export const Secondary = () => (
+  <>
+    <Button variant="secondary">Secondary</Button>
+    <Button variant="secondary" disabled>
+      Disabled
+    </Button>
+    <Button variant="secondary" isWaiting={true} waitingText="Waiting...">
+      Button
+    </Button>
+  </>
+);
 
-export const Linkish = () => <>
-  <ButtonLink onClick={() => alert('hi')}>press to say hi</ButtonLink>
-</>;
+export const Link = () => (
+  <>
+    <LinkButton href="https://openstax.org">Primary</LinkButton>
+    <LinkButton variant="secondary" href="https://openstax.org" target="_blank">
+      Button
+    </LinkButton>
+  </>
+);
+
+export const Linkish = () => (
+  <>
+    <ButtonLink onClick={() => alert('hi')}>press to say hi</ButtonLink>
+  </>
+);
